25c4e62e47d1f6c1165b688d71906c8834012f71
Sam Reid <reids@colorado.edu>
2015-03-04 14:01:05 -0700

    Removed showHomeScreen=false when in ?dev mode

--- js/energy-forms-and-changes-main.js
+++ js/energy-forms-and-changes-main.js
@@ -30,14 +30,6 @@ define( function( require ) {
     }
   };
 
-  // Appending '?dev' to the URL will enable developer-only features.
-  if ( phet.chipper.getQueryParameter( 'dev' ) ) {
-    simOptions = _.extend( {
-      // add dev-specific options here
-      showHomeScreen: false
-    }, simOptions );
-  }
-
   SimLauncher.launch( function() {
 
     var sim = new Sim( simTitle, [ new EnergyFormsAndChangesIntroScreen(), new EnergyFormsAndChangesEnergySystemsScreen() ], simOptions );
