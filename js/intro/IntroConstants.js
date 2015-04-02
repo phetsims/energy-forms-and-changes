b9208c1c021d0021faa187ef90feee7302ed7727
jessegreenberg <jesse.greenberg@colorado.edu>
2015-04-02 01:27:34 -0600

    Create IntroConstants to hold the constants in the Java that were shared between model and view elements.  This removes circular dependencies between model and view elements.

---
+++ js/intro/IntroConstants.js
@@ -0,0 +1,17 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Constants used in the Intro screen of Energy Forms and Changes.
+ * @author Jesse Greenberg
+ */
+define( function( require ) {
+  'use strict';
+
+  return {
+
+    // constants used by burners
+    BURNER_EDGE_TO_HEIGHT_RATIO: 0.2, // Multiplier empirically determined for best look.
+    PERSPECTIVE_ANGLE: Math.PI / 4 // Positive is counterclockwise, a value of 0 produces a non-skewed rectangle.
+
+  };
+} );
