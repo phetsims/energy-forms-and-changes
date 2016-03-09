e70091bc8974217ded4d235ef239b063d79e42de
Andrew Adare <andrewadare@gmail.com>
2016-03-09 11:41:13 -0700

    Initial commit

---
+++ js/energy-systems/model/PositionableFadableModelElement.js
@@ -0,0 +1,30 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Base class for model elements that can be positioned and faded.
+ *
+ * @author  Andrew Adare
+ * @author  Jesse Greenberg
+ */
+define( function( require ) {
+  'use strict';
+
+  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var PositionableModelElement = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/PositionableModelElement' );
+
+  /**
+   * Abstract positionable fadable model element class
+   *
+   * @param {Vector2} initialPosition Initial position of element
+   * @constructor
+   */
+  function PositionableFadableModelElement( initialPosition, initialOpacity ) {
+
+    PositionableModelElement.call( this, initialPosition );
+
+    this.addProperty( 'opacity', initialOpacity );
+  }
+
+  return inherit( PropertySet, PositionableFadableModelElement );
+} );
