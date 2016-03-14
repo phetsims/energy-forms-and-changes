3cfbda360aaaa0d57633fd6bf5b839990edcde3b
Andrew Adare <andrewadare@gmail.com>
2016-03-14 13:39:18 -0600

    Adding Cloud.js

---
+++ js/energy-systems/model/Cloud.js
@@ -0,0 +1,50 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Module representing a cloud to block energy from the sun.
+ *
+ * @author  John Blanco
+ * @author  Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // Modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var Shape = require( 'KITE/Shape' );
+
+  // Constants
+  var CLOUD_WIDTH = 0.035; // In meters, though obviously not to scale.  Empirically determined.
+  var CLOUD_HEIGHT = CLOUD_WIDTH; // TODO: Java code uses ModelElementImage. Equivalent in JS?
+
+  function Cloud( offsetFromParent, parentPosition ) {
+    this.offsetFromParent = offsetFromParent;
+    this.parentPosition = parentPosition;
+    this.addProperty( 'existenceStrength', 1.0 );
+  }
+
+  return inherit( PropertySet, Cloud, {
+
+    /**
+     * Return ellipse with size of Cloud
+     *
+     * @return {Shape.ellipse} Ellipse with axes sized to width and height of cloud
+     */
+    getCloudAbsorptionReflectionShape: function() {
+      var x = this.parentPosition.x + this.offsetFromParent.x - CLOUD_WIDTH / 2;
+      var y = this.parentPosition.y + this.offsetFromParent.y - CLOUD_HEIGHT / 2;
+      return Shape.ellipse( x, y, CLOUD_WIDTH, CLOUD_HEIGHT, 0, 0, 0, false );
+    },
+
+    /**
+     * Return (x,y) position of center of cloud
+     *
+     * @return {Vector2} Center position of cloud
+     */
+    getCenterPosition: function() {
+      return this.parentPosition.plus( this.offsetFromParent );
+    }
+
+  } );
+} );
