f3ecde8eea60377d572ab0076329a215d2ebb3fd
Andrew Adare <andrewadare@gmail.com>
2016-03-14 11:54:31 -0600

    Adding Energy.js

---
+++ js/energy-systems/model/Energy.js
@@ -0,0 +1,29 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Module representing an amount and type of energy as well as other
+ * attributes that are specific to the energy type.
+ *
+ * @author  John Blanco
+ * @author  Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  /**
+   * @param {EnergyType} type      Energy type.
+   * @param {Number}     amount    Amount of energy, in joules.
+   * @param {Number}     direction Direction of energy, in radians.  Not meaningful for
+   *                               all energy types.  Zero indicates to the right, PI/2
+   *                               is up, and so forth.
+   */
+  function Energy( type, amount, direction ) {
+    this.type = type;
+    this.amount = amount;
+    this.direction = direction;
+  }
+
+  return inherit( Object, Energy );
+} );
