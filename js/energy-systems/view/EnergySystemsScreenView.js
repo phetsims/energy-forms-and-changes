1795749aef05d97601ec171bbcec37732a036717
Andrew Adare <andrewadare@gmail.com>
2016-03-15 13:12:27 -0600

    Add energy chunk check box. Factor all drawn items into functions

--- js/energy-systems/view/EnergySystemsScreenView.js
+++ js/energy-systems/view/EnergySystemsScreenView.js
@@ -11,19 +11,33 @@
 define( function( require ) {
   'use strict';
 
-  // modules
+  // Modules
   var Bounds2 = require( 'DOT/Bounds2' );
+  var CheckBox = require( 'SUN/CheckBox' );
+  var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
+  var EnergyChunkLegend = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/EnergyChunkLegend' );
+  var EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
+  var EnergyType = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyType' );
   var HSlider = require( 'SUN/HSlider' );
   var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var LayoutBox = require( 'SCENERY/nodes/LayoutBox' );
+  var Panel = require( 'SUN/Panel' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Property = require( 'AXON/Property' );
-  var ScreenView = require( 'JOIST/ScreenView' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
-  var EnergyChunkLegend = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/EnergyChunkLegend' );
+  var ScreenView = require( 'JOIST/ScreenView' );
+  var Text = require( 'SCENERY/nodes/Text' );
   var Vector2 = require( 'DOT/Vector2' );
 
-  // images
+  // Images
   var mockupImage = require( 'image!ENERGY_FORMS_AND_CHANGES/mockup_energy_systems.png' );
+
+  // Strings
+  var energySymbolsString = require( 'string!ENERGY_FORMS_AND_CHANGES/energySymbols' );
+
+  // Constants
+  var EDGE_INSET = 10;
 
   /**
    * @param {EnergySystemsModel} model
@@ -35,36 +49,76 @@ define( function( require ) {
       layoutBounds: new Bounds2( 0, 0, 768, 504 )
     } );
 
-    //Show the mock-up and a slider to change its transparency
-    var mockupOpacityProperty = new Property( 0.8 );
-    var image = new Image( mockupImage, {
-      pickable: false
-    } );
-    image.scale( this.layoutBounds.width / image.width );
-    mockupOpacityProperty.linkAttribute( image, 'opacity' );
-    this.addChild( image );
-    this.addChild( new HSlider( mockupOpacityProperty, {
-      min: 0,
-      max: 1
-    }, {
-      top: 10,
-      left: 10
-    } ) );
+    var thisScreenView = this;
 
-    // Create the legend for energy chunks.
-    var energyChunkLegend = new EnergyChunkLegend();
-    energyChunkLegend.center = new Vector2( 0.9*this.layoutBounds.width , 0.5*this.layoutBounds.height );
-    this.addChild( energyChunkLegend );
+    //Show the mock-up and a slider to change its transparency
+    function addMockupImage() {
+      var opacity = new Property( 0.8 );
+      var image = new Image( mockupImage, {
+        pickable: false
+      } );
+      image.scale( thisScreenView.layoutBounds.width / image.width );
+      opacity.linkAttribute( image, 'opacity' );
+      thisScreenView.addChild( image );
+      thisScreenView.addChild( new HSlider( opacity, {
+        min: 0,
+        max: 1
+      }, {
+        top: 10,
+        left: 10
+      } ) );
+    }
+
+    // Create the legend for energy chunk types
+    function addEnergyChunkLegend() {
+      var legend = new EnergyChunkLegend();
+      var x = 0.9 * thisScreenView.layoutBounds.width;
+      var y = 0.5 * thisScreenView.layoutBounds.height;
+      legend.center = new Vector2( x, y );
+      thisScreenView.addChild( legend );
+    }
+
+    // Check box panel to display energy chunks
+    function addCheckBoxPanel() {
+      var label = new Text( energySymbolsString, {
+        font: new PhetFont( 20 )
+      } );
+
+      var energyChunkNode = EnergyChunkNode.createEnergyChunkNode( EnergyType.THERMAL );
+      energyChunkNode.scale( 1.0 );
+      energyChunkNode.pickable = false;
+
+      var checkBox = new CheckBox( new LayoutBox( {
+        children: [ label, energyChunkNode ],
+        orientation: 'horizontal',
+        spacing: 5
+      } ), model.energyChunksVisibleProperty );
+
+      var panel = new Panel( checkBox, {
+        fill: EFACConstants.CONTROL_PANEL_BACKGROUND_COLOR,
+        stroke: EFACConstants.CONTROL_PANEL_OUTLINE_STROKE,
+        lineWidth: EFACConstants.CONTROL_PANEL_OUTLINE_LINE_WIDTH
+      } );
+      panel.rightTop = new Vector2( thisScreenView.layoutBounds.width - EDGE_INSET, EDGE_INSET );
+      thisScreenView.addChild( panel );
+    }
 
     // Create and add the Reset All Button in the bottom right, which resets the model
-    var resetAllButton = new ResetAllButton( {
-      listener: function() {
-        model.reset();
-      },
-      right: this.layoutBounds.maxX - 10,
-      bottom: this.layoutBounds.maxY - 10
-    } );
-    this.addChild( resetAllButton );
+    function addResetButton() {
+      var resetAllButton = new ResetAllButton( {
+        listener: function() {
+          model.reset();
+        },
+        right: thisScreenView.layoutBounds.maxX - 10,
+        bottom: thisScreenView.layoutBounds.maxY - 10
+      } );
+      thisScreenView.addChild( resetAllButton );
+    }
+
+    addMockupImage();
+    addEnergyChunkLegend();
+    addCheckBoxPanel();
+    addResetButton();
 
   }
 
