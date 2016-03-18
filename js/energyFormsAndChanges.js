c8f6aa4beb57c2b779361f691f88686058f11597
Andrew Adare <andrewadare@gmail.com>
2016-03-18 10:43:25 -0600

    Add energyFormsAndChanges.js

---
+++ js/energyFormsAndChanges.js
@@ -0,0 +1,15 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Create energyFormsAndChanges namespace
+ * @author  Jesse Greenberg
+ * @author  Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // Modules
+  var Namespace = require( 'PHET_CORE/Namespace' );
+
+  return new Namespace( 'energyFormsAndChanges' );
+} );
