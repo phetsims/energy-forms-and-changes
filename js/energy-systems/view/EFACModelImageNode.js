78156da2b4ba99630cce9bec0a05df9cad0b5d55
Andrew Adare <andrewadare@gmail.com>
2016-03-21 17:36:23 -0600

    Add EFACModelImageNode.js

---
+++ js/energy-systems/view/EFACModelImageNode.js
@@ -0,0 +1,42 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Class that represents an image-based model element, or a piece thereof, in
+ * the view.
+ *
+ * @author John Blanco
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // Modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+
+  /**
+   * @param {EFACModelImage} modelImage     [description]
+   * @param {ModelViewTransform} modelViewTransform [description]
+   * @constructor
+   */
+  function EFACModelImageNode( modelImage, modelViewTransform ) {
+
+    Node.call( this );
+
+    var img = new Image( modelImage.image );
+    var scale = modelViewTransform.modelToViewDeltaX( modelImage.width ) / this.width;
+    var offset = modelViewTransform.modelToViewDelta( modelImage.centerToCenterOffset );
+
+    if ( scale !== 1 ) {
+      this.setScaleMagnitude( scale );
+    }
+
+    // TODO port this line
+    // centerFullBoundsOnPoint( centerToCenterOffsetInView.getX(), centerToCenterOffsetInView.getY() );
+    console.log(offset);
+    
+    this.addChild( img );
+  }
+
+  return inherit( Node, EFACModelImageNode );
+} );
