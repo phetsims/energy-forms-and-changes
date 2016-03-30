aa0b03a25977544e9f9cbb225265f14d50aac76b
Andrew Adare <andrewadare@gmail.com>
2016-03-30 16:54:34 -0600

    Draw belt with arcs

--- js/energy-systems/view/BeltNode.js
+++ js/energy-systems/view/BeltNode.js
@@ -35,9 +35,6 @@ define( function( require ) {
     var wheel2Radius = modelViewTransform.modelToViewDeltaX( belt.wheel2Radius );
     var wheel2Center = modelViewTransform.modelToViewPosition( belt.wheel2Center );
 
-    // var wheel1Shape = Shape.ellipse( wheel1Center.x, wheel1Center.y, wheel1Radius, wheel1Radius );
-    // var wheel2Shape = Shape.ellipse( wheel2Center.x, wheel2Center.y, wheel2Radius, wheel2Radius );
-
     var wheel1CenterToWheelTwoCenter = wheel2Center.minus( wheel1Center );
     var wheel1ToOneEdge = wheel1CenterToWheelTwoCenter.perpendicular().withMagnitude( wheel1Radius );
     var wheel2ToOneEdge = wheel1CenterToWheelTwoCenter.perpendicular().withMagnitude( wheel2Radius );
@@ -49,39 +46,27 @@ define( function( require ) {
     points.push( wheel2Center.minus( wheel2ToOneEdge ) );
     points.push( wheel2Center.plus( wheel2ToOneEdge ) );
 
-    var overallShape = new Shape();
-    overallShape.moveToPoint(points[0]);
-    overallShape.lineToPoint(points[1]);
-    overallShape.lineToPoint(points[2]);
-    overallShape.lineToPoint(points[3]);
-    overallShape.close();
+    var beltShape = new Shape();
+    var arcAngle = wheel1ToOneEdge.angle() + Math.PI;
 
-    var overallPath = new Path(overallShape, {
+    // Arc around bike wheel
+    beltShape.arc( wheel1Center.x, wheel1Center.y, wheel1Radius, arcAngle, arcAngle+Math.PI, false );
+    beltShape.moveToPoint( points[ 1 ] ); // bottom of bike wheel
+    beltShape.lineToPoint( points[ 2 ] ); // bottom of generator wheel
+
+    // Arc around generator wheel
+    beltShape.arc( wheel2Center.x, wheel2Center.y, wheel2Radius, arcAngle, arcAngle+Math.PI, true );
+    beltShape.moveToPoint( points[ 3 ] ); // top of generator wheel
+    beltShape.lineToPoint( points[ 0 ] ); // top of bike wheel
+
+    var beltPath = new Path( beltShape, {
       stroke: BELT_COLOR,
       lineWidth: 5,
       lineCap: 'butt',
       lineJoin: 'round'
-    });
-    this.addChild(overallPath);
-    //     // Create a shape that will connect the two circles in a belt-like shape.
-    // List<Vector2D> points = new ArrayList<Vector2D>() {{
-    //     Vector2D wheel1CenterToWheelTwoCenter = wheel2Center.minus( wheel1Center );
-    //     Vector2D wheel1ToOneEdge = wheel1CenterToWheelTwoCenter.getPerpendicularVector().getInstanceOfMagnitude( wheel1Radius );
-    //     add( wheel1Center.plus( wheel1ToOneEdge ) );
-    //     add( wheel1Center.minus( wheel1ToOneEdge ) );
-    //     Vector2D wheel2ToOneEdge = wheel1CenterToWheelTwoCenter.getPerpendicularVector().getInstanceOfMagnitude( wheel2Radius );
-    //     add( wheel2Center.minus( wheel2ToOneEdge ) );
-    //     add( wheel2Center.plus( wheel2ToOneEdge ) );
-    // }};
+    } );
 
-    // var sunPath = new Path( sunShape, {
-    //   fill: Color.YELLOW,
-    //   lineWidth: 1,
-    //   stroke: Color.YELLOW
-    // } );
-
-    // sunPath.setTranslation( modelViewTransform.modelToViewDelta( SunEnergySource.OFFSET_TO_CENTER_OF_SUN ) );
-
+    this.addChild( beltPath );
   }
 
   energyFormsAndChanges.register( 'BeltNode', BeltNode );
