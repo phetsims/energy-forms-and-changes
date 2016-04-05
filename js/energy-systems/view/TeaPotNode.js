0acc28ab5003a4aae75aa0dbfe8799dd54057cf5
Andrew Adare <andrewadare@gmail.com>
2016-04-05 12:37:09 -0600

    Remove dependence on Burner model component.

--- js/energy-systems/view/TeaPotNode.js
+++ js/energy-systems/view/TeaPotNode.js
@@ -4,9 +4,8 @@ define( function( require ) {
   'use strict';
 
   // Modules
-  var Burner = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/Burner' );
   var BurnerStandNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/BurnerStandNode' );
-  var EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
+  // var EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
   var EFACBaseNode = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/EFACBaseNode' );
   var EFACModelImageNode = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/EFACModelImageNode' );
   var HeaterCoolerFront = require( 'SCENERY_PHET/HeaterCoolerFront' );
@@ -14,7 +13,9 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var TeaPot = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/TeaPot' );
   var Vector2 = require( 'DOT/Vector2' );
+  var Bounds2 = require( 'DOT/Bounds2' );
 
+  var BURNER_MODEL_BOUNDS = new Bounds2( -0.0375, 0, 0.0375, 0.075 ); // From Burner.getBoundingRect()
   var BURNER_EDGE_TO_HEIGHT_RATIO = 0.2; // Multiplier empirically determined for best look.
 
   /**
@@ -25,28 +26,23 @@ define( function( require ) {
    */
   function TeaPotNode( teaPot, energyChunksVisible, modelViewTransform ) {
 
-
     EFACBaseNode.call( this, teaPot, modelViewTransform );
 
     var teaPotImageNode = new EFACModelImageNode( TeaPot.TEAPOT_IMAGE, modelViewTransform );
 
-    // Burner model component for use in heater-cooler
-    var burner = new Burner( new Vector2( 0, 0 ), energyChunksVisible );
-
-    // Burner stand node
-    var burnerSize = modelViewTransform.modelToViewShape( burner.getOutlineRect() );
-    var burnerProjection = burnerSize.width * BURNER_EDGE_TO_HEIGHT_RATIO;
-    var burnerStandNode = new BurnerStandNode( burnerSize, burnerProjection );
-
     // Node for heater-cooler bucket.
     // Front and back are added separately so support layering of energy chunks.
     var heaterCoolerBack = new HeaterCoolerBack( {
-      heatCoolLevelProperty: burner.heatCoolLevelProperty
+      heatCoolLevelProperty: teaPot.heatCoolAmountProperty
     } );
     var heaterCoolerFront = new HeaterCoolerFront( {
-      heatCoolLevelProperty: burner.heatCoolLevelProperty
+      heatCoolLevelProperty: teaPot.heatCoolAmountProperty
     } );
 
+    // Burner stand node
+    var burnerSize = modelViewTransform.modelToViewShape( BURNER_MODEL_BOUNDS );
+    var burnerProjection = burnerSize.width * BURNER_EDGE_TO_HEIGHT_RATIO;
+    var burnerStandNode = new BurnerStandNode( burnerSize, burnerProjection );
 
     burnerStandNode.centerTop = teaPotImageNode.centerBottom.plus( new Vector2( 0, -teaPotImageNode.height / 4 ) );
     heaterCoolerBack.centerTop = teaPotImageNode.centerBottom.plus( new Vector2( 0, teaPotImageNode.height / 4 ) );
