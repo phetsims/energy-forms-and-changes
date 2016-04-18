2dc4c2dfb6d7aefe58661e33e454ed959775ad23
Andrew Adare <andrewadare@gmail.com>
2016-04-18 12:15:37 -0600

    Begin porting LightRayNode functions (untested)

--- js/energy-systems/view/LightRayNode.js
+++ js/energy-systems/view/LightRayNode.js
@@ -12,10 +12,25 @@ define( function( require ) {
   'use strict';
 
   // Modules
-  // Replace with correct path
   var Node = require( 'REPO/path/to/Node' );
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Ray2 = require( 'DOT/Ray2' );
+
+  var STROKE_THICKNESS = 2;
+  var SEARCH_ITERATIONS = 10;
+  var FADE_COEFFICIENT_IN_AIR = 0.005;
+
+  /**
+   * @param {Vector2} point     position
+   * @param {Number} fadeValue Fade coefficient
+   * @constructor
+   * @private
+   */
+  function PointAndFadeValue( point, fadeValue ) {
+    this.point = point;
+    this.fadeValue = fadeValue;
+  }
 
   /**
    * @param {Vector2} origin
@@ -24,12 +39,18 @@ define( function( require ) {
    * @constructor
    */
   function LightRayNode( origin, endpoint, color ) {
+
+    this.lightAbsorbingShapes = [];
+    this.pointAndFadeValues = [];
     this.origin = origin;
     this.endpoint = endpoint;
     this.color = color;
 
-    // Add args to constructor as needed
     Node.call( this );
+
+    var self = this;
+
+    this.updateLineSegments();
   }
 
   energyFormsAndChanges.register( 'LightRayNode', LightRayNode );
@@ -41,7 +62,12 @@ define( function( require ) {
      * @param {LightAbsorbingShape} lightAbsorbingShape [description]
      * @public
      */
-    addLightAbsorbingShape: function( lightAbsorbingShape ) {},
+    addLightAbsorbingShape: function( lightAbsorbingShape ) {
+      lightAbsorbingShape.absorptionCoefficientProperty.link( function() {
+        this.updateLineSegments()
+      } );
+      this.lightAbsorbingShapes.push( lightAbsorbingShape );
+    },
 
     /**
      * [removeLightAbsorbingShape description]
@@ -49,12 +75,34 @@ define( function( require ) {
      * @return {[type]}
      * @public
      */
-    removeLightAbsorbingShape: function( lightAbsorbingShape ) {},
+    removeLightAbsorbingShape: function( lightAbsorbingShape ) {
+      lightAbsorbingShape.absorptionCoefficientProperty.unlinkAll();
+      _.remove( this.lightAbsorbingShapes, lightAbsorbingShape );
+      this.updateLineSegments();
+    },
 
     /**
      * @private
      */
-    updateLineSegments: function() {},
+    updateLineSegments: function() {
+      this.removeAllChildren();
+      this.pointAndFadeValues.length = 0;
+
+      this.pointAndFadeValues.push( new PointAndFadeValue( this.origin, FADE_COEFFICIENT_IN_AIR ) );
+      this.pointAndFadeValues.push( new PointAndFadeValue( this.endpoint, 0 ) );
+
+      this.lightAbsorbingShapes.foreach( function( absorbingShape ) {
+        if ( self.lineIntersectsShapeIntersects( self.origin, self.endpoint, absorbingShape.shape ) ) {
+          var entryPoint =
+        }
+      } );
+
+
+
+
+
+
+    },
 
     /**
      * [lineIntersectsShapeIntersects description]
@@ -64,7 +112,14 @@ define( function( require ) {
      * @return {Boolean}            [description]
      * @private
      */
-    lineIntersectsShapeIntersects: function( startPoint, endPoint, shape ) {},
+    lineIntersectsShapeIntersects: function( startPoint, endPoint, shape ) {
+      var direction = endpoint.minus( startPoint ).normalized();
+      var ray = new Ray2( startPoint, direction );
+      return shape.windingIntersection( ray );
+      // Warning: This only checks the bounding rect, not the full shape.
+      // This was adequate in this case, but take care if reusing.
+      // return shape.bounds.intersectsLine( startPoint.x, startPoint.y, endPoint.x, endPoint.y );
+    },
 
     /**
      * [getLineIntersection description]
@@ -82,7 +137,35 @@ define( function( require ) {
      * @return {Vector2}
      * @private
      */
-    getShapeEntryPoint: function( origin, endpoint, shape ) {},
+    getShapeEntryPoint: function( origin, endpoint, shape ) {
+      var shapeRect = shape.bounds;
+      var entryPoint = null;
+      var ray = new Ray2( origin, endpoint.minus( origin ).normalized() );
+      if ( shapeRect.windingIntersection( ray ) ) {
+        var boundsEntryPoint = this.getRectangleEntryPoint( origin, endpoint, shapeRect );
+        if ( boundsEntryPoint === null ) {
+          return null;
+        }
+        var boundsExitPoint = this.getRectangleExitPoint( origin, endpoint, shapeRect );
+        var searchEndPoint = boundsExitPoint == null ? endpoint : boundsExitPoint;
+
+
+        // Search linearly for edge of the shape.  BIG HAIRY NOTE - This
+        // will not work in all cases.  It worked for the coarse shapes
+        // and rough bounds needed for this simulation.  Don't reuse if you
+        // need good general edge finding.
+        var angle = endpoint.minus( origin ).angle;
+        var incrementalDistance = boundsEntryPoint.distance( searchEndPoint ) / SEARCH_ITERATIONS;
+        for ( var i = 0; i < SEARCH_ITERATIONS; i++ ) {
+          var testPoint = boundsEntryPoint.plus( new Vector2( incrementalDistance * i, 0 ).rotated( angle ) );
+          if ( shape.contains( testPoint ) ) {
+            entryPoint = testPoint;
+            break;
+          }
+        }
+      }
+      return entryPoint;
+    },
 
     /**
      * @param  {Vector2} origin   [description]
@@ -91,7 +174,32 @@ define( function( require ) {
      * @return {Vector2}
      * @private
      */
-    getShapeExitPoint: function( origin, endpoint, shape ) {},
+    getShapeExitPoint: function( origin, endpoint, shape ) {
+      var shapeRect = shape.bounds;
+      var exitPoint = null;
+
+      if ( shape.contains( endpoint ) ) {
+        // Line ends inside shape, return null.
+        return null;
+      }
+
+      var ray = new Ray2( origin, endpoint.minus( origin ).normalized() );
+      if ( !shape.contains( endpoint ) && shapeRect.intersectsLine( ray ) ) {
+        // Phase I - Do a binary search to locate the edge of the
+        // rectangle that encloses the shape.
+        var angle = endpoint.minus( origin ).angle;
+        var length = origin.distance( endpoint );
+        var lengthChange = length / 2;
+        for ( var i = 0; i < SEARCH_ITERATIONS; i++ ) {
+          var start = origin.plus( new Vector2( length, 0 ).rotated( angle ) );
+          var testLine = new Line( start.x, start.y, endpoint.x, endpoint.y );
+          length += lengthChange * ( testLine.intersects( shapeRect ) ? 1 : -1 );
+          lengthChange = lengthChange / 2;
+        }
+        exitPoint = origin.plus( new Vector2( length, 0 ).rotated( angle ) );
+      }
+      return exitPoint;
+    },
 
 
     /**
