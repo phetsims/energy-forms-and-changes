67e15c9865c48f94a7622038072988cf5f247879
Andrew Adare <andrewadare@gmail.com>
2016-04-27 15:50:17 -0600

    BeakerHeater energy chunk functions

--- js/energy-systems/model/BeakerHeater.js
+++ js/energy-systems/model/BeakerHeater.js
@@ -7,7 +7,8 @@ define( function( require ) {
   var Beaker = require( 'ENERGY_FORMS_AND_CHANGES/common/model/Beaker' );
   var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
   var EFACModelImage = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/EFACModelImage' );
-  // var EnergyChunk = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyChunk' );
+  var EnergyChunk = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyChunk' );
+  var EnergyChunkPathMover = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/EnergyChunkPathMover' );
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   var EnergyType = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyType' );
   var EnergyUser = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/EnergyUser' );
@@ -15,7 +16,7 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Image = require( 'SCENERY/nodes/Image' );
   var ObservableArray = require( 'AXON/ObservableArray' );
-  // var Random = require( 'DOT/Random' );
+  var Random = require( 'DOT/Random' );
   var Vector2 = require( 'DOT/Vector2' );
 
   // Images
@@ -36,23 +37,23 @@ define( function( require ) {
   var HEATER_ELEMENT_OFF_IMAGE = new EFACModelImage( HEATER_ELEMENT_DARK, HEATER_ELEMENT_OFFSET );
   var HEATER_ELEMENT_ON_IMAGE = new EFACModelImage( HEATER_ELEMENT, HEATER_ELEMENT_OFFSET );
 
-  // var OFFSET_TO_LEFT_SIDE_OF_WIRE = new Vector2( -0.04, -0.04 );
-  // var OFFSET_TO_LEFT_SIDE_OF_WIRE_BEND = new Vector2( -0.02, -0.04 );
-  // var OFFSET_TO_FIRST_WIRE_CURVE_POINT = new Vector2( -0.01, -0.0375 );
-  // var OFFSET_TO_SECOND_WIRE_CURVE_POINT = new Vector2( -0.001, -0.025 );
-  // var OFFSET_TO_THIRD_WIRE_CURVE_POINT = new Vector2( -0.0005, -0.0175 );
-  // var OFFSET_TO_BOTTOM_OF_CONNECTOR = new Vector2( 0, -0.01 );
-  // var OFFSET_TO_CONVERSION_POINT = new Vector2( 0, 0.012 );
+  var OFFSET_TO_LEFT_SIDE_OF_WIRE = new Vector2( -0.04, -0.04 );
+  var OFFSET_TO_LEFT_SIDE_OF_WIRE_BEND = new Vector2( -0.02, -0.04 );
+  var OFFSET_TO_FIRST_WIRE_CURVE_POINT = new Vector2( -0.01, -0.0375 );
+  var OFFSET_TO_SECOND_WIRE_CURVE_POINT = new Vector2( -0.001, -0.025 );
+  var OFFSET_TO_THIRD_WIRE_CURVE_POINT = new Vector2( -0.0005, -0.0175 );
+  var OFFSET_TO_BOTTOM_OF_CONNECTOR = new Vector2( 0, -0.01 );
+  var OFFSET_TO_CONVERSION_POINT = new Vector2( 0, 0.012 );
 
-  // var RAND = new Random();
+  var RAND = new Random();
   var BEAKER_WIDTH = 0.075; // In meters.
   var BEAKER_HEIGHT = BEAKER_WIDTH * 0.9;
   var BEAKER_OFFSET = new Vector2( 0, 0.025 );
   // var THERMOMETER_OFFSET = new Vector2( 0.033, 0.035 );
-  // var HEATING_ELEMENT_ENERGY_CHUNK_VELOCITY = 0.0075; // In meters/sec, quite slow.
-  // var HEATER_ELEMENT_2D_HEIGHT = HEATER_ELEMENT_OFF_IMAGE.height;
+  var HEATING_ELEMENT_ENERGY_CHUNK_VELOCITY = 0.0075; // In meters/sec, quite slow.
+  var HEATER_ELEMENT_2D_HEIGHT = HEATER_ELEMENT_OFF_IMAGE.height;
   var MAX_HEAT_GENERATION_RATE = 5000; // Joules/sec, not connected to incoming energy.
-  // var RADIATED_ENERGY_CHUNK_TRAVEL_DISTANCE = 0.2; // In meters.
+  var RADIATED_ENERGY_CHUNK_TRAVEL_DISTANCE = 0.2; // In meters.
   var HEAT_ENERGY_CHANGE_RATE = 0.5; // In proportion per second.
 
   /**
@@ -64,10 +65,13 @@ define( function( require ) {
     EnergyUser.call( this, new Image( WATER_ICON ) );
     this.energyChunksVisibleProperty = energyChunksVisibleProperty;
 
+    // var self = this;
+
     this.addProperty( 'heatProportion', 0 );
     this.electricalEnergyChunkMovers = [];
     this.heatingElementEnergyChunkMovers = [];
     this.radiatedEnergyChunkMovers = [];
+    this.radiatedEnergyChunkList = new ObservableArray();
 
     this.beaker = new Beaker( BEAKER_OFFSET, BEAKER_WIDTH, BEAKER_HEIGHT, energyChunksVisibleProperty );
 
@@ -77,16 +81,13 @@ define( function( require ) {
     //   }
     // }, THERMOMETER_OFFSET, true );
 
-    this.radiatedEnergyChunkList = new ObservableArray();
 
-    // // Update the position of the beaker and thermometer when the overall
-    // // model element position changes.
-    // getObservablePosition().addObserver( new VoidFunction1 < Vector2D > () {
-    //   public void apply( Vector2D position ) {
-    //     beaker.position.set( position.plus( BEAKER_OFFSET ) );
-    //     thermometer.position.set( position.plus( THERMOMETER_OFFSET ) );
-    //   }
-    // } );
+    // Update the position of the beaker and thermometer when the overall
+    // model element position changes.
+    this.positionProperty.link( function( position ) {
+      // self.beaker.positionProperty.set( position.plus( BEAKER_OFFSET ) );
+      // thermometer.position.set( position.plus( THERMOMETER_OFFSET ) );
+    } );
 
   }
 
@@ -174,7 +175,24 @@ define( function( require ) {
      * @param  {Number} dt timestep
      * @private
      */
-    moveRadiatedEnergyChunks: function( dt ) {},
+    moveRadiatedEnergyChunks: function( dt ) {
+      var self = this;
+      this.radiatedEnergyChunkMovers.forEach( function( mover ) {
+        mover.moveAlongPath( dt );
+
+        if ( mover.pathFullyTraversed ) {
+
+          // Remove this energy chunk entirely.
+          _.remove( self.radiatedEnergyChunkList, function( chunk ) {
+            return chunk === mover.energyChunk;
+          } );
+
+          _.remove( self.radiatedEnergyChunkMovers, function( m ) {
+            return m === mover;
+          } );
+        }
+      } );
+    },
 
     /**
      * [description]
@@ -182,7 +200,29 @@ define( function( require ) {
      * @param  {Number} dt timestep
      * @private
      */
-    moveThermalEnergyChunks: function( dt ) {},
+    moveThermalEnergyChunks: function( dt ) {
+      var self = this;
+      this.heatingElementEnergyChunkMovers.forEach( function( mover ) {
+        mover.moveAlongPath( dt );
+
+        if ( mover.pathFullyTraversed ) {
+
+          // The chunk is ready to move to the beaker.  We remove it
+          // from here, and the beaker takes over management of the
+          // chunk.
+          self.beaker.addEnergyChunk( mover.energyChunk );
+
+          _.remove( self.energyChunkList, function( chunk ) {
+            return chunk === mover.energyChunk;
+          } );
+
+          _.remove( self.heatingElementEnergyChunkMovers, function( m ) {
+            return m === mover;
+          } );
+
+        }
+      } );
+    },
 
     /**
      * [description]
@@ -190,7 +230,28 @@ define( function( require ) {
      * @param  {Number} dt timestep
      * @private
      */
-    moveElectricalEnergyChunks: function( dt ) {},
+    moveElectricalEnergyChunks: function( dt ) {
+      var self = this;
+      this.electricalEnergyChunkMovers.forEach( function( mover ) {
+        mover.moveAlongPath( dt );
+
+        if ( mover.pathFullyTraversed ) {
+
+          // The electrical energy chunk has reached the burner, so
+          // it needs to change into thermal energy.
+          _.remove( self.electricalEnergyChunkMovers, function( m ) {
+            return m === mover;
+          } );
+          mover.energyChunk.energyTypeProperty.set( EnergyType.THERMAL );
+
+          // Have the thermal energy move a little on the element
+          // before moving into the beaker.
+          self.heatingElementEnergyChunkMovers.push( new EnergyChunkPathMover( mover.energyChunk,
+            self.createHeaterElementEnergyChunkPath( mover.energyChunk.positionProperty.get() ),
+            HEATING_ELEMENT_ENERGY_CHUNK_VELOCITY ) );
+        }
+      } );
+    },
 
     /**
      * [preLoadEnergyChunks description]
@@ -199,7 +260,51 @@ define( function( require ) {
      * @public
      * @override
      */
-    preLoadEnergyChunks: function( incomingEnergyRate ) {},
+    preLoadEnergyChunks: function( incomingEnergyRate ) {
+      this.clearEnergyChunks();
+
+      if ( incomingEnergyRate.amount === 0 || incomingEnergyRate.type !== EnergyType.ELECTRICAL ) {
+        // No energy chunk pre-loading needed.
+        return;
+      }
+
+      var dt = 1 / EFACConstants.FRAMES_PER_SECOND;
+      var energySinceLastChunk = EFACConstants.ENERGY_PER_CHUNK * 0.99;
+
+      // Simulate energy chunks moving through the system.
+      var preLoadComplete = false;
+      while ( !preLoadComplete ) {
+        energySinceLastChunk += incomingEnergyRate.amount * dt;
+
+        // Determine if time to add a new chunk.
+        if ( energySinceLastChunk >= EFACConstants.ENERGY_PER_CHUNK ) {
+
+          // Create and add a new chunk
+          var newEnergyChunk = new EnergyChunk(
+            EnergyType.ELECTRICAL,
+            this.positionProperty.get().plus( OFFSET_TO_LEFT_SIDE_OF_WIRE ),
+            Vector2.ZERO,
+            this.energyChunksVisibleProperty );
+          this.energyChunkList.push( newEnergyChunk );
+
+          // Add a "mover" that will move this energy chunk through
+          // the wire to the heating element.
+          this.electricalEnergyChunkMovers.push( new EnergyChunkPathMover( newEnergyChunk,
+            this.createElectricalEnergyChunkPath( this.positionProperty.get() ),
+            EFACConstants.ENERGY_CHUNK_VELOCITY ) );
+
+          // Update energy since last chunk.
+          energySinceLastChunk -= EFACConstants.ENERGY_PER_CHUNK;
+        }
+
+        this.moveElectricalEnergyChunks( dt );
+
+        if ( this.heatingElementEnergyChunkMovers.length > 0 ) {
+          // An energy chunk has made it to the heating element, which completes the preload.
+          preLoadComplete = true;
+        }
+      }
+    },
 
     /**
      * Reset some variables/properties when deactivated by carousel
@@ -212,7 +317,15 @@ define( function( require ) {
       this.beaker.reset();
     },
 
-    clearEnergyChunks: function() {},
+    clearEnergyChunks: function() {
+      EnergyUser.prototype.clear.call( this );
+
+      this.electricalEnergyChunkMovers.length = 0;
+      this.heatingElementEnergyChunkMovers.length = 0;
+      this.radiatedEnergyChunkMovers.length = 0;
+
+      this.radiatedEnergyChunkList.clear();
+    },
 
     /**
      * [description]
@@ -222,7 +335,17 @@ define( function( require ) {
      * @return {Array<Vector2>} [description]
      * @private
      */
-    createHeaterElementEnergyChunkPath: function( startingPoint ) {},
+    createHeaterElementEnergyChunkPath: function( startingPoint ) {
+      var path = [];
+
+      // The path for the thermal energy chunks is meant to look like it
+      // is moving on the burner element.  This must be manually
+      // coordinated with the burner element image.
+      var angle = RAND.nextBoolean() ? RAND.nextDouble() * Math.PI * 0.45 : -RAND.nextDouble() * Math.PI * 0.3;
+      path.push( startingPoint.plus( new Vector2( 0, HEATER_ELEMENT_2D_HEIGHT ).rotated( angle ) ) );
+
+      return path;
+    },
 
     /**
      * [description]
@@ -232,17 +355,45 @@ define( function( require ) {
      * @return {Array<Vector2>} [description]
      * @private
      */
-    createElectricalEnergyChunkPath: function( center ) {},
+    createElectricalEnergyChunkPath: function( center ) {
+      var path = [];
+
+      path.push( center.plus( OFFSET_TO_LEFT_SIDE_OF_WIRE_BEND ) );
+      path.push( center.plus( OFFSET_TO_FIRST_WIRE_CURVE_POINT ) );
+      path.push( center.plus( OFFSET_TO_SECOND_WIRE_CURVE_POINT ) );
+      path.push( center.plus( OFFSET_TO_THIRD_WIRE_CURVE_POINT ) );
+      path.push( center.plus( OFFSET_TO_BOTTOM_OF_CONNECTOR ) );
+      path.push( center.plus( OFFSET_TO_CONVERSION_POINT ) );
+
+      return path;
+    },
 
     /**
-     * [description]
+     * Path for chunks to take when radiated from the beaker
      *
      * @param  {Vector2} startingPoint [description]
-     *
      * @return {Array<Vector2>} [description]
      * @private
      */
-    createRadiatedEnergyChunkPath: function( startingPoint ) {}
+    createRadiatedEnergyChunkPath: function( startingPoint ) {
+      var path = [];
+      var numDirectionChanges = 4; // Empirically chosen.
+      var nominalTravelVector = new Vector2( 0, RADIATED_ENERGY_CHUNK_TRAVEL_DISTANCE / numDirectionChanges );
+
+      // The first point is straight above the start point.  This just looks
+      // good, making the chunk move straight up out of the beaker.
+      var currentPosition = startingPoint.plus( nominalTravelVector );
+      path.push( currentPosition );
+
+      // Add the remaining points in the path.
+      for ( var i = 0; i < numDirectionChanges - 1; i++ ) {
+        var movement = nominalTravelVector.rotated( ( RAND.nextDouble() - 0.5 ) * Math.PI / 4 );
+        currentPosition = currentPosition.plus( movement );
+        path.push( currentPosition );
+      }
+
+      return path;
+    }
 
   }, {
     WIRE_STRAIGHT_IMAGE: WIRE_STRAIGHT_IMAGE,
