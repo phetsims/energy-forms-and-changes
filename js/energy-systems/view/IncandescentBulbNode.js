6ad3189feb5577adbd370a23860a6ec4745a919e
Andrew Adare <andrewadare@gmail.com>
2016-05-27 14:18:10 -0600

    Update IncandescentBulbNode

--- js/energy-systems/view/IncandescentBulbNode.js
+++ js/energy-systems/view/IncandescentBulbNode.js
@@ -10,16 +10,17 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var Color = require( 'SCENERY/util/Color' );
+  var EnergyChunkLayer = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkLayer' );
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   var EFACBaseNode = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/EFACBaseNode' );
   var LightBulb = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/LightBulb' );
+  var LightRays = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/LightRays' );
   var IncandescentBulb = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/model/IncandescentBulb' );
   var EFACModelImageNode = require( 'ENERGY_FORMS_AND_CHANGES/energy-systems/view/EFACModelImageNode' );
-
-  // constants
+  var Vector2 = require( 'DOT/Vector2' );
 
   /**
-   *
    * @param {IncandescentBulb} lightBulb
    * @param {Property.<boolean>} energyChunksVisibeProperty
    * @param {ModelViewTransform2} modelViewTransform
@@ -29,16 +30,33 @@ define( function( require ) {
 
     EFACBaseNode.call( this, lightBulb, modelViewTransform );
 
-    // TODO: Add light rays from Java
-    // Add the images and the layer that will contain the energy chunks.
+    var lightRays = new LightRays( Vector2.ZERO, 30, 400, 20, Color.YELLOW );
+    this.addChild( lightRays );
 
+    // Only show the light rays when the energy chunks are not shown.
+    energyChunksVisibleProperty.link( function( visible ) {
+      lightRays.setVisible( !visible );
+    } );
+
+    // Add the images and the layer that will contain the energy chunks.
     this.addChild( new EFACModelImageNode( LightBulb.WIRE_FLAT_IMAGE, modelViewTransform ) );
     this.addChild( new EFACModelImageNode( LightBulb.WIRE_CURVE_IMAGE, modelViewTransform ) );
     this.addChild( new EFACModelImageNode( LightBulb.ELEMENT_BASE_BACK_IMAGE, modelViewTransform ) );
-    // addChild( new EnergyChunkLayer( lightBulb.energyChunkList, lightBulb.getObservablePosition(), mvt ) );
+    this.addChild( new EnergyChunkLayer( lightBulb.energyChunkList, lightBulb.positionProperty, modelViewTransform ) );
     this.addChild( new EFACModelImageNode( LightBulb.ELEMENT_BASE_FRONT_IMAGE, modelViewTransform ) );
     this.addChild( new EFACModelImageNode( IncandescentBulb.NON_ENERGIZED_BULB, modelViewTransform ) );
-    // var energizedBulb = this.addChild( new EFACModelImageNode( IncandescentLightBulb.ENERGIZED_BULB, modelViewTransform ) );
+
+    var energizedBulb = new EFACModelImageNode( IncandescentBulb.ENERGIZED_BULB, modelViewTransform );
+    this.addChild( energizedBulb );
+
+    // Center the light rays on the bulb image.
+    lightRays.centerY = -energizedBulb.bounds.height * 0.18;
+
+    // Update the transparency of the lit bulb based on model element.
+    lightBulb.litProportionProperty.link( function( litProportion ) {
+      energizedBulb.setOpacity( litProportion );
+      lightRays.setOpacity( litProportion );
+    } );
 
   }
 
@@ -46,3 +64,4 @@ define( function( require ) {
 
   return inherit( EFACBaseNode, IncandescentBulbNode, {} );
 } );
+
