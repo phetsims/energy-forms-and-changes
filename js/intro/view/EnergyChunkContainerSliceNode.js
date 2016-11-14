18aa98a28f5e26e607bb7b4f899374f49a796ea6
Andrew Adare <andrewadare@gmail.com>
2016-11-14 15:52:45 -0700

    Add listeners to existing as well as future chunks (#23)

--- js/intro/view/EnergyChunkContainerSliceNode.js
+++ js/intro/view/EnergyChunkContainerSliceNode.js
@@ -6,13 +6,13 @@
  * effect of layering in order to get a bit of a 3D appearance.
  *
  * @author John Blanco
+ * @author Andrew Adare
  */
 
 define( function( require ) {
   'use strict';
 
   // modules
-
   var EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -23,37 +23,37 @@ define( function( require ) {
   var ALWAYS_SHOW_OUTLINE = false; // DEBUG
 
   /**
-   * *
-   * @param {EnergyChunkContainerSlice} energyChunkContainerSlice
+   * @param {EnergyChunkContainerSlice} slice
    * @param {ModelViewTransform2} modelViewTransform
    * @constructor
    */
-  function EnergyChunkContainerSliceNode( energyChunkContainerSlice, modelViewTransform ) {
+  function EnergyChunkContainerSliceNode( slice, modelViewTransform ) {
 
     var self = this;
     this.modelViewTransform = modelViewTransform;
-    this.energyChunkContainerSlice = energyChunkContainerSlice;
+    this.energyChunkContainerSlice = slice;
     Node.call( this );
 
-    // Add the initial energy chunks.
-    // this.addInitialEnergyChunks(); // AA: No!
-
-    energyChunkContainerSlice.energyChunkList.addItemAddedListener( function( addedEnergyChunk ) {
-      var energyChunkNode = new EnergyChunkNode( addedEnergyChunk, modelViewTransform );
-      // console.log( 'ECCSN: Adding energyChunkNode' );
+    var listener = function( addedChunk ) {
+      var energyChunkNode = new EnergyChunkNode( addedChunk, modelViewTransform );
       self.addChild( energyChunkNode );
-      energyChunkContainerSlice.energyChunkList.addItemRemovedListener( function removalListener( removedEnergyChunk ) {
-        if ( removedEnergyChunk === addedEnergyChunk ) {
-          // console.log( 'ECCSN: Removing energyChunkNode' );
+      slice.energyChunkList.addItemRemovedListener( function removalListener( removedChunk ) {
+        if ( removedChunk === addedChunk ) {
           self.removeChild( energyChunkNode );
-          energyChunkContainerSlice.energyChunkList.removeItemRemovedListener( removalListener );
+          slice.energyChunkList.removeItemRemovedListener( removalListener );
         }
       } );
-    } );
+    };
+
+    // Add listeners to any existing chunks in the slice
+    slice.energyChunkList.forEach( listener );
+
+    // Add listeners to future chunks when added
+    slice.energyChunkList.addItemAddedListener( listener );
 
     // For debug.
     if ( ALWAYS_SHOW_OUTLINE ) {
-      this.addChild( new Path( modelViewTransform.modelToViewShape( energyChunkContainerSlice.shape ), {
+      this.addChild( new Path( modelViewTransform.modelToViewShape( slice.shape ), {
         lineWidth: 1,
         stroke: 'red'
       } ) );
@@ -62,17 +62,5 @@ define( function( require ) {
 
   energyFormsAndChanges.register( 'EnergyChunkContainerSliceNode', EnergyChunkContainerSliceNode );
 
-  return inherit( Node, EnergyChunkContainerSliceNode, {
-
-    // Note (AA): this function does not belong here. See #23. Leaving intact until more progress
-    // is made on that issue.
-    addInitialEnergyChunks: function() {
-      var self = this;
-      this.energyChunkContainerSlice.energyChunkList.forEach( function( energyChunk ) {
-        self.addChild( new EnergyChunkNode( energyChunk, self.modelViewTransform ) );
-      } );
-    }
-  } );
-
+  return inherit( Node, EnergyChunkContainerSliceNode );
 } );
-
