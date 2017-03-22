f8910ab672cf81c683387c2ffa6ed8b9b73c255b
samreid <reids@colorado.edu>
2017-03-22 09:47:43 -0600

    Added missing require parameter, see https://github.com/phetsims/chipper/issues/554

--- js/intro/model/EnergyContainerCategory.js
+++ js/intro/model/EnergyContainerCategory.js
@@ -7,7 +7,7 @@
  * @author John Blanco
  */
 
-define( function() {
+define( function( require ) {
   'use strict';
 
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
