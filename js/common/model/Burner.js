a69d98d6b3d983faa2e5d89833beb78f36c87754
jbphet <john.blanco@colorado.edu>
2018-03-21 14:46:16 -0600

    code cleanup

--- js/common/model/Burner.js
+++ js/common/model/Burner.js
@@ -40,28 +40,35 @@ define( function( require ) {
   // Units: joules/sec, multiplier empirically chosen.
   var MAX_ENERGY_GENERATION_RATE_INTO_AIR = MAX_ENERGY_GENERATION_RATE * 0.3;
 
-  // counter used by constructor to create unique IDs
+  // counter used by constructor to create unique IDs for each burner
   var idCounter = 0;
 
   /**
-   * Burner class
-   *
-   * @param {Vector2} position - The position in model space where this burner exists.
-   * @param {Property.<boolean>} energyChunksVisibleProperty - Controls whether the energy chunks are visible
+   * @param {Vector2} position - the position in model space where this burner exists
+   * @param {Property.<boolean>} energyChunksVisibleProperty - controls whether the energy chunks are visible
    * @constructor
    */
   function Burner( position, energyChunksVisibleProperty ) {
 
+    var self = this;
     ModelElement.call( this );
+
+    // @public (read-only) {string} - unique ID, used for debug
     this.id = 'burner-' + idCounter++;
 
+    // @public {Property<number>}
     this.heatCoolLevelProperty = new Property( 0 );
 
-    var self = this;
-
-    this.position = position;
+    // @public (read-only) {ObservableArray.<EnergyChunk>}
     this.energyChunkList = new ObservableArray();
+
+    // @private {Vector2}
+    this.position = position;
+
+    // @private {EnergyChunkWanderController[]}
     this.energyChunkWanderControllers = [];
+
+    // @private {Property.<boolean>}
     this.energyChunksVisibleProperty = energyChunksVisibleProperty;
 
     // Track energy transferred to anything sitting on the burner.
@@ -70,16 +77,18 @@ define( function( require ) {
     // Track build up of energy for transferring chunks to/from the air.
     this.energyExchangedWithAirSinceLastChunkTransfer = 0; // @private
 
-    // Create and add the top surface.  Some compensation for perspective is
-    // necessary in order to avoid problems with edge overlap when dropping
-    // objects on top of burner.
-    var perspectiveCompensation =
-      self.getOutlineRect().height * EFACConstants.BURNER_EDGE_TO_HEIGHT_RATIO *
-      Math.cos( EFACConstants.BURNER_PERSPECTIVE_ANGLE );
+    // Create and add the top surface.  Some compensation for perspective is necessary in order to avoid problems with
+    // edge overlap when dropping objects on top of burner.
+    var perspectiveCompensation = self.getOutlineRect().height * EFACConstants.BURNER_EDGE_TO_HEIGHT_RATIO *
+                                  Math.cos( EFACConstants.BURNER_PERSPECTIVE_ANGLE );
 
+    // @public (read-only) {Property.<HorizontalSurface>} - surface upon which other objects can rest
     this.topSurfaceProperty = new Property(
-      new HorizontalSurface( new RangeWithValue( self.getOutlineRect().getMinX() - perspectiveCompensation,
-        self.getOutlineRect().maxX + perspectiveCompensation ), self.getOutlineRect().maxY, this ) );
+      new HorizontalSurface(
+        new RangeWithValue( self.getOutlineRect().getMinX() - perspectiveCompensation,
+          self.getOutlineRect().maxX + perspectiveCompensation ), self.getOutlineRect().maxY, this
+      )
+    );
   }
 
   energyFormsAndChanges.register( 'Burner', Burner );
@@ -88,16 +97,16 @@ define( function( require ) {
 
     /**
      * Get a rectangle that defines the outline of the burner.  In the model the burner is essentially a 2D rectangle.
-     *
-     * @returns {Rectangle} Rectangle that defines the outline in model space.
+     * @returns {Rectangle} - rectangle that defines the outline in model space.
+     * @public
      */
     getOutlineRect: function() {
       return new Rectangle( this.position.x - SIDE_LENGTH / 2, this.position.y, SIDE_LENGTH, SIDE_LENGTH );
     },
 
     /**
-     * *
      * @returns {Property}
+     * @public
      */
     getTopSurfaceProperty: function() {
       return this.topSurfaceProperty;
@@ -105,12 +114,13 @@ define( function( require ) {
 
     /**
      * Interact with a thermal energy container, adding or removing energy based on the current heat/cool setting.
-     *
-     * @param {ThermalEnergyContainer} thermalEnergyContainer - Model object that will get or give energy.
-     * @param {number} dt - Amount of time (delta time).
+     * @param {ThermalEnergyContainer} thermalEnergyContainer - model object that will get or give energy
+     * @param {number} dt - amount of time (delta time)
+     * @public
      */
     addOrRemoveEnergyToFromObject: function( thermalEnergyContainer, dt ) {
-      // This shouldn't be used for air - there is a specific method for that.
+
+      // this shouldn't be used for air - there is a specific method for that
       var deltaEnergy = 0;
 
       if ( this.inContactWith( thermalEnergyContainer ) ) {
@@ -123,11 +133,11 @@ define( function( require ) {
     },
 
     /**
-     * Add or remove energy from the specific energy container air.  This has specific behavior that is different from
-     * addOremoveEnergyToFromObject() above.
-     *
+     * Exchange energy with the air.  This has specific behavior that is different from addOrRemoveEnergyToFromObject,
+     * defined elsewhere in this type.
      * @param {Air} air - air as a thermal energy container
      * @param dt - amount of time (delta time)
+     * @public
      */
     addOrRemoveEnergyToFromAir: function( air, dt ) {
       var deltaEnergy = MAX_ENERGY_GENERATION_RATE_INTO_AIR * this.heatCoolLevelProperty.value * dt;
@@ -139,10 +149,10 @@ define( function( require ) {
     },
 
     /**
-     * Determine if the burner is in contact with a thermal energy container.
-     *
+     * determine if the burner is in contact with a thermal energy container
      * @param {ThermalEnergyContainer} thermalEnergyContainer
      * @returns {boolean}
+     * @public
      */
     inContactWith: function( thermalEnergyContainer ) {
       var burnerRect = this.getOutlineRect();
@@ -153,44 +163,54 @@ define( function( require ) {
     },
 
     /**
-     *
      * @param {EnergyChunk} energyChunk
+     * @public
      */
     addEnergyChunk: function( energyChunk ) {
-      var controller = new EnergyChunkWanderController( energyChunk,
-        new Property( this.getEnergyChunkStartEndPoint() ), null );
+
+      // create a controller that will move this energy chunk around
+      var controller = new EnergyChunkWanderController(
+        energyChunk,
+        new Property( this.getEnergyChunkStartEndPoint() ),
+        null
+      );
 
       energyChunk.zPosition = 0;
+
+      // add the chunk and its controller to this model
       this.energyChunkList.add( energyChunk );
       this.energyChunkWanderControllers.push( controller );
+
+      // reset energy transfer accumulators
       this.energyExchangedWithAirSinceLastChunkTransfer = 0;
       this.energyExchangedWithObjectSinceLastChunkTransfer = 0;
     },
 
     /**
-     * @private
      * @returns {Vector2}
+     * @private
      */
     getEnergyChunkStartEndPoint: function() {
       return this.getCenterPoint();
     },
 
     /**
-     * Request an energy chunk from the burner.
-     *
-     * @param {Vector2} point - Point from which to search for closest chunk.
-     * @returns {EnergyChunk} Closest energy chunk, null if none are contained.
+     * request an energy chunk from the burner
+     * @param {Vector2} point - point from which to search for closest chunk
+     * @returns {EnergyChunk} - closest energy chunk, null if none are contained
+     * @public
      */
     extractClosestEnergyChunk: function( point ) {
-      // Extend the scope for callbacks.
       var self = this;
       var closestEnergyChunk = null;
       if ( this.energyChunkList.length > 0 ) {
         this.energyChunkList.forEach( function( energyChunk ) {
           if ( energyChunk.positionProperty.value.distance( self.position ) > ENERGY_CHUNK_CAPTURE_DISTANCE &&
-            ( closestEnergyChunk === null ||
-              energyChunk.positionProperty.value.distance( point ) < closestEnergyChunk.positionProperty.value.distance( point ) ) ) {
-            // Found a closer chunk.
+               ( closestEnergyChunk === null ||
+                 energyChunk.positionProperty.value.distance( point ) <
+                 closestEnergyChunk.positionProperty.value.distance( point ) ) ) {
+
+            // found a closer chunk
             closestEnergyChunk = energyChunk;
           }
         } );
@@ -204,22 +224,26 @@ define( function( require ) {
       }
 
       if ( closestEnergyChunk === null && this.heatCoolLevelProperty.value > 0 ) {
-        // Create an energy chunk.
+
+        // create an energy chunk
         closestEnergyChunk = new EnergyChunk( EnergyType.THERMAL, this.getEnergyChunkStartEndPoint(),
           new Vector2( 0, 0 ), this.energyChunksVisibleProperty );
       }
       if ( closestEnergyChunk !== null ) {
         this.energyExchangedWithAirSinceLastChunkTransfer = 0;
         this.energyExchangedWithObjectSinceLastChunkTransfer = 0;
-      } else {
+      }
+      else {
+
+        // TODO: This was in the Java code, and will be left for a while, but should be removed or turned into an assert eventually.
         console.log( 'Warning: Request for energy chunk from burner when not in heat mode and no chunks contained, returning null.' );
       }
       return closestEnergyChunk;
     },
 
     /**
-     * *
      * @returns {Vector2}
+     * @public
      */
     getCenterPoint: function() {
       return new Vector2( this.position.x, this.position.y + SIDE_LENGTH / 2 );
@@ -237,9 +261,9 @@ define( function( require ) {
     },
 
     /**
-     * *
      * @param {ThermalEnergyContainer[]} thermalEnergyContainers
      * @returns {boolean}
+     * @public
      */
     areAnyOnTop: function( thermalEnergyContainers ) {
       var self = this; // Provide a handle for use in nested callback
@@ -253,8 +277,8 @@ define( function( require ) {
     },
 
     /**
-     * *
      * @returns {number}
+     * @public
      */
     getEnergyChunkCountForAir: function() {
       var self = this; // extend scope for nested loop function.
@@ -276,10 +300,9 @@ define( function( require ) {
     },
 
     /**
-     * Animate the energy chunks.
-     *
-     * @private
+     * animate the energy chunks
      * @param {number} dt
+     * @private
      */
     step: function( dt ) {
       var self = this;
@@ -298,11 +321,13 @@ define( function( require ) {
     },
 
     /**
-     * *
+     * get a rectangle in model space the corresponds to where the flame and ice exist
      * @returns {Rectangle}
+     * @public
      */
     getFlameIceRect: function() {
-      // be coordinated with the view.
+
+      // word of warning: this needs to stay consistent with the view
       var outlineRect = this.getOutlineRect();
       var width = outlineRect.width;
       var height = outlineRect.height;
@@ -310,9 +335,9 @@ define( function( require ) {
     },
 
     /**
-     * Get burner temperature, clamped at minimum to the freezing point of water.
-     *
+     * get burner temperature, clamped at minimum to the freezing point of water
      * @returns {number}
+     * @public
      */
     getTemperature: function() {
       var temperature = EFACConstants.ROOM_TEMPERATURE + this.heatCoolLevelProperty.value * 100;
@@ -320,11 +345,10 @@ define( function( require ) {
     },
 
     /**
-     * Get the (signed) number of energy chunks for interaction with thermal
-     * objects (as opposed to air).
-     *
-     * @return Number of energy chunks that could be supplied or consumed.
-     *         Negative value indicates that chunks should come in.
+     * get the (signed) number of energy chunks for interaction with thermal objects (as opposed to air)
+     * @return {number} - The umber of energy chunks that could be supplied or consumed. Negative value indicates that
+     * chunks should come in.
+     * @public
      */
     getEnergyChunkBalanceWithObjects: function() {
       var deltaE = this.energyExchangedWithObjectSinceLastChunkTransfer;
@@ -333,17 +357,16 @@ define( function( require ) {
     },
 
     /**
-     * Whether burner can emit an energy chunk to another element
-     *
+     * whether burner can emit an energy chunk to another element
      * @returns {boolean}
+     * @public
      */
     canSupplyEnergyChunk: function() {
       return this.heatCoolLevelProperty.value > 0;
     },
 
     /**
-     * Whether burner can receive an energy chunk from another element
-     *
+     * whether burner can receive an energy chunk from another element
      * @returns {boolean}
      */
     canAcceptEnergyChunk: function() {
