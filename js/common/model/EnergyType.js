e8f1d35c656acd568e55a2ebafb42822588cdd79
jbphet <john.blanco@colorado.edu>
2018-03-21 16:08:54 -0600

    code cleanup

--- js/common/model/EnergyType.js
+++ js/common/model/EnergyType.js
@@ -1,26 +1,27 @@
 // Copyright 2014-2017, University of Colorado Boulder
 
 /**
- * The various types of energy used in this simulation.
- *
+ * enum that represents the various types of energy used in this simulation
  * @author John Blanco
  */
-
 define( function( require ) {
   'use strict';
 
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
 
-  var EnergyType = Object.freeze( {
+  var EnergyType = {
     THERMAL: 'THERMAL',
     ELECTRICAL: 'ELECTRICAL',
     MECHANICAL: 'MECHANICAL',
     LIGHT: 'LIGHT',
     CHEMICAL: 'CHEMICAL',
     HIDDEN: 'HIDDEN'
-  } );
+  };
 
   energyFormsAndChanges.register( 'EnergyType', EnergyType );
+
+  // verify that enum is immutable, without the runtime penalty in production code
+  if ( assert ) { Object.freeze( EnergyType ); }
 
   return EnergyType;
 } );
