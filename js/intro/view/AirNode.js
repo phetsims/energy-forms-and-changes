f0cb8fdfaa8c62cf7a4b56e46f8663bb7f34163f
jbphet <john.blanco@colorado.edu>
2018-04-27 10:25:26 -0600

    code cleanup

--- js/intro/view/AirNode.js
+++ js/intro/view/AirNode.js
@@ -1,28 +1,31 @@
 // Copyright 2014-2017, University of Colorado Boulder
 
 /**
+ * view representation of the air, which is basically just a parent node through which energy chunks can move
+ *
  * @author John Blanco
  */
 define( function( require ) {
   'use strict';
+
+  // modules
   var EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
 
-
   // constants
-  var SHOW_BOUNDS = false;
+  var SHOW_BOUNDS = false; // useful for debugging
 
   /**
-   *
-   * @param {Air} air
+   * @param {Air} air - model of the air
    * @param {ModelViewTransform2} modelViewTransform
    * @constructor
    */
   function AirNode( air, modelViewTransform ) {
 
+    var self = this;
     Node.call( this );
 
     if ( SHOW_BOUNDS ) {
@@ -32,20 +35,13 @@ define( function( require ) {
       } ) );
     }
 
-    // var EnergyChunkLayer = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkLayer' );
-    // this.addChild( new EnergyChunkLayer( air.energyChunkList, air.positionProperty, modelViewTransform ) );
-
-    // Create a layer where energy chunks will be placed.
-    var energyChunkLayer = new Node();
-    this.addChild( energyChunkLayer );
-
-    // Watch for energy chunks coming and going and add/remove nodes accordingly.
+    // watch for energy chunks coming and going and add/remove nodes accordingly
     air.energyChunkList.addItemAddedListener( function( addedEnergyChunk ) {
       var energyChunkNode = new EnergyChunkNode( addedEnergyChunk, modelViewTransform );
-      energyChunkLayer.addChild( energyChunkNode );
+      self.addChild( energyChunkNode );
       air.energyChunkList.addItemRemovedListener( function removalListener( removedEnergyChunk ) {
         if ( removedEnergyChunk === addedEnergyChunk ) {
-          energyChunkLayer.removeChild( energyChunkNode );
+          self.removeChild( energyChunkNode );
           air.energyChunkList.removeItemRemovedListener( removalListener );
         }
       } );
