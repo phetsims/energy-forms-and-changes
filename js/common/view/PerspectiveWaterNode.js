48e3870ea1f935e8c4ec163b7f0da65ffdfc927d
jbphet <john.blanco@colorado.edu>
2018-07-23 16:37:22 -0600

    changed to use a reusable bounds instead of recreating a rectangle on each step, see #38 and #42

--- js/common/view/PerspectiveWaterNode.js
+++ js/common/view/PerspectiveWaterNode.js
@@ -9,8 +9,8 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var Bounds2 = require( 'DOT/Bounds2' );
   var Circle = require( 'SCENERY/nodes/Circle' );
-  var DotRectangle = require( 'DOT/Rectangle' ); // eslint-disable-line require-statement-match
   var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
   var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -51,6 +51,9 @@ define( function( require ) {
     this.waterLevelProperty = waterLevelProperty;
     this.temperatureProperty = temperatureProperty;
 
+    // @private - a rectangle that defines the size of the fluid within the beaker
+    this.fluidBounds = Bounds2.NOTHING.copy();
+
     // @private - nodes that represent the top and body of the water
     this.liquidWaterTopNode = new Path( null, {
       fill: EFACConstants.WATER_COLOR_IN_BEAKER.colorUtilsBrighter( 0.25 ),
@@ -76,6 +79,13 @@ define( function( require ) {
 
     // update the appearance of the water as the level changes
     this.waterLevelProperty.link( function( waterLevel ) {
+      var waterHeight = beakerOutlineRect.height * waterLevel;
+      self.fluidBounds.setMinMax(
+        beakerOutlineRect.minX,
+        beakerOutlineRect.maxY - waterHeight,
+        beakerOutlineRect.maxX,
+        0
+      );
       self.updateWaterAppearance( waterLevel, beakerOutlineRect );
     } );
   }
@@ -107,19 +117,12 @@ define( function( require ) {
      * @param {Rectangle} beakerOutlineRect
      * @private
      */
-    updateWaterAppearance: function( fluidLevel, beakerOutlineRect ) {
-      var waterHeight = beakerOutlineRect.height * fluidLevel;
-      var liquidWaterRect = new DotRectangle(
-        beakerOutlineRect.minX,
-        beakerOutlineRect.maxY - waterHeight,
-        beakerOutlineRect.width,
-        waterHeight
-      );
-      var ellipseWidth = beakerOutlineRect.width;
+    updateWaterAppearance: function() {
+      var ellipseWidth = this.fluidBounds.width;
       var ellipseHeight = PERSPECTIVE_PROPORTION * ellipseWidth;
       var liquidWaterTopEllipse = Shape.ellipse(
-        liquidWaterRect.centerX,
-        liquidWaterRect.minY,
+        this.fluidBounds.centerX,
+        this.fluidBounds.minY,
         ellipseWidth / 2,
         ellipseHeight / 2,
         0,
@@ -128,13 +131,13 @@ define( function( require ) {
         false
       );
 
-      var halfWidth = liquidWaterRect.width / 2;
+      var halfWidth = this.fluidBounds.width / 2;
       var halfHeight = ellipseHeight / 2;
       var liquidWaterBodyShape = new Shape()
-        .moveTo( liquidWaterRect.minX, liquidWaterRect.minY ) // Top left of the beaker body.
-        .ellipticalArc( liquidWaterRect.centerX, liquidWaterRect.minY, halfWidth, halfHeight, 0, Math.PI, 0, false )
-        .lineTo( liquidWaterRect.maxX, liquidWaterRect.maxY ) // Bottom right of the beaker body.
-        .ellipticalArc( liquidWaterRect.centerX, liquidWaterRect.maxY, halfWidth, halfHeight, 0, 0, Math.PI, false )
+        .moveTo( this.fluidBounds.minX, this.fluidBounds.minY ) // Top left of the beaker body.
+        .ellipticalArc( this.fluidBounds.centerX, this.fluidBounds.minY, halfWidth, halfHeight, 0, Math.PI, 0, false )
+        .lineTo( this.fluidBounds.maxX, this.fluidBounds.maxY ) // Bottom right of the beaker body.
+        .ellipticalArc( this.fluidBounds.centerX, this.fluidBounds.maxY, halfWidth, halfHeight, 0, 0, Math.PI, false )
         .close();
 
       this.liquidWaterBodyNode.setShape( liquidWaterBodyShape );
@@ -153,26 +156,18 @@ define( function( require ) {
 
       var self = this; // extend scope for nested callbacks.
 
-      var waterHeight = beakerOutlineRect.height * fluidLevel;
-      var liquidWaterRect = new DotRectangle(
-        beakerOutlineRect.minX,
-        beakerOutlineRect.maxY - waterHeight,
-        beakerOutlineRect.width,
-        waterHeight
-      );
-
       var steamingProportion = 0;
       if ( EFACConstants.BOILING_POINT_TEMPERATURE - temperature < STEAMING_RANGE ) {
 
         // the water is emitting some amount of steam - set the proportionate amount
-        steamingProportion = 1 - ((EFACConstants.BOILING_POINT_TEMPERATURE - temperature) / STEAMING_RANGE);
+        steamingProportion = 1 - ( ( EFACConstants.BOILING_POINT_TEMPERATURE - temperature ) / STEAMING_RANGE );
         steamingProportion = Util.clamp( steamingProportion, 0, 1 );
       }
 
       // add any new steam bubbles
       if ( steamingProportion > 0 ) {
         var bubblesToProduceCalc =
-          (STEAM_BUBBLE_RATE_RANGE.min + STEAM_BUBBLE_RATE_RANGE.getLength() * steamingProportion) * dt;
+          ( STEAM_BUBBLE_RATE_RANGE.min + STEAM_BUBBLE_RATE_RANGE.getLength() * steamingProportion ) * dt;
         var bubblesToProduce = Math.floor( bubblesToProduceCalc );
 
         this.bubbleProductionRemainder += bubblesToProduceCalc - bubblesToProduce;
@@ -185,9 +180,9 @@ define( function( require ) {
         for ( var i = 0; i < bubblesToProduce; i++ ) {
           var steamBubbleDiameter = STEAM_BUBBLE_DIAMETER_RANGE.min +
                                     phet.joist.random.nextDouble() * STEAM_BUBBLE_DIAMETER_RANGE.getLength();
-          var steamBubbleCenterXPos = beakerOutlineRect.centerX +
-                                      (phet.joist.random.nextDouble() - 0.5) *
-                                      (beakerOutlineRect.width - steamBubbleDiameter);
+          var steamBubbleCenterXPos = this.fluidBounds.centerX +
+                                      ( phet.joist.random.nextDouble() - 0.5 ) *
+                                      ( this.fluidBounds.width - steamBubbleDiameter );
 
           var steamBubble = new Circle( steamBubbleDiameter / 2, {
             fill: 'white',
@@ -195,7 +190,7 @@ define( function( require ) {
           } );
 
           // bubbles are invisible to start; they will fade in
-          steamBubble.center = new Vector2( steamBubbleCenterXPos, liquidWaterRect.getMinY() );
+          steamBubble.center = new Vector2( steamBubbleCenterXPos, this.fluidBounds.getMinY() );
           steamBubble.opacity = 0;
           this.steamBubbleNodes.push( steamBubble );
           this.steamNode.addChild( steamBubble );
@@ -204,7 +199,7 @@ define( function( require ) {
 
       // update the position and appearance of the existing steam bubbles
       var steamBubbleSpeed = STEAM_BUBBLE_SPEED_RANGE.min + steamingProportion * STEAM_BUBBLE_SPEED_RANGE.getLength();
-      var unfilledBeakerHeight = beakerOutlineRect.height - waterHeight;
+      var unfilledBeakerHeight = this.beakerOutlineRect.height - this.fluidBounds.height;
 
       this.steamBubbleNodes.forEach( function( steamBubble ) {
 
@@ -219,21 +214,21 @@ define( function( require ) {
 
         // update position of floating bubbles
         else if ( steamBubble.y < beakerOutlineRect.minY ) {
-          steamBubble.setRadius( steamBubble.bounds.width * (1 + (STEAM_BUBBLE_GROWTH_RATE * dt)) / 2 );
+          steamBubble.setRadius( steamBubble.bounds.width * ( 1 + ( STEAM_BUBBLE_GROWTH_RATE * dt ) ) / 2 );
           var distanceFromCenterX = steamBubble.x - beakerOutlineRect.centerX;
 
           // give bubbles some lateral drift motion
           steamBubble.translate( distanceFromCenterX * 0.2 * dt, 0 );
 
           // fade the bubble as it reaches the end of its range
-          var heightFraction = (beakerOutlineRect.minY - steamBubble.y) / MAX_STEAM_BUBBLE_HEIGHT;
-          steamBubble.opacity = (1 - heightFraction) * MAX_STEAM_BUBBLE_OPACITY;
+          var heightFraction = ( beakerOutlineRect.minY - steamBubble.y ) / MAX_STEAM_BUBBLE_HEIGHT;
+          steamBubble.opacity = ( 1 - heightFraction ) * MAX_STEAM_BUBBLE_OPACITY;
         }
 
         // fade new bubbles in
         else {
-          var distanceFromWater = liquidWaterRect.y - steamBubble.y;
-          var opacityFraction = Util.clamp( distanceFromWater / (unfilledBeakerHeight / 4), 0, 1 );
+          var distanceFromWater = self.fluidBounds.minY - steamBubble.y;
+          var opacityFraction = Util.clamp( distanceFromWater / ( unfilledBeakerHeight / 4 ), 0, 1 );
           steamBubble.opacity = opacityFraction * MAX_STEAM_BUBBLE_OPACITY;
         }
       } );
