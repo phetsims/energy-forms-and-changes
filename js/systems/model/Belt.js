0701bbdb214da3b404ba997fd7f7ae839666a841
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-06 15:39:00 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/model/Belt.js
+++ js/systems/model/Belt.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * A model element that represents a belt that connects two rotating wheels together, like a fan belt in an automobile
@@ -11,65 +11,64 @@
  * @author John Blanco
  * @author Andrew Adare
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Property = require( 'AXON/Property' );
-  var Shape = require( 'KITE/Shape' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Property = require( 'AXON/Property' );
+  const Shape = require( 'KITE/Shape' );
 
-  /**
-   * @param {number} wheel1Radius
-   * @param {Vector2} wheel1Center
-   * @param {number} wheel2Radius
-   * @param {Vector2} wheel2Center
-   * @constructor
-   */
-  function Belt( wheel1Radius, wheel1Center, wheel2Radius, wheel2Center ) {
+  class Belt {
 
-    // Calculate the angle needed to make the belt look like it contacts the wheels at the right places, see
-    // https://github.com/phetsims/energy-forms-and-changes/issues/98.
-    var contactAngle = Math.asin( ( wheel2Radius - wheel1Radius ) / wheel1Center.distance( wheel2Center ) );
+    /**
+     * @param {number} wheel1Radius
+     * @param {Vector2} wheel1Center
+     * @param {number} wheel2Radius
+     * @param {Vector2} wheel2Center
+     */
+    constructor( wheel1Radius, wheel1Center, wheel2Radius, wheel2Center ) {
 
-    // vectors needed to create the belt shape
-    var wheel1CenterToWheelTwoCenter = wheel2Center.minus( wheel1Center );
-    var wheel1CenterToArcEnd =
-      wheel1CenterToWheelTwoCenter.perpendicular().withMagnitude( wheel1Radius ).rotated( -contactAngle );
-    var wheel1CenterToArcStart = wheel1CenterToArcEnd.rotated( Math.PI + 2 * contactAngle );
-    var wheel2CenterToArcStart =
-      wheel1CenterToWheelTwoCenter.perpendicular().withMagnitude( wheel2Radius ).rotated( -contactAngle );
-    var wheel2CenterToArcEnd = wheel2CenterToArcStart.rotated( Math.PI + 2 * contactAngle );
+      // Calculate the angle needed to make the belt look like it contacts the wheels at the right places, see
+      // https://github.com/phetsims/energy-forms-and-changes/issues/98.
+      const contactAngle = Math.asin( ( wheel2Radius - wheel1Radius ) / wheel1Center.distance( wheel2Center ) );
 
-    // create the shape of the belt
-    var beltShape = new Shape();
-    beltShape.moveToPoint( wheel1Center.plus( wheel1CenterToArcStart ) );
-    beltShape.arcPoint(
-      wheel1Center,
-      wheel1Radius,
-      wheel1CenterToArcStart.angle(),
-      wheel1CenterToArcEnd.angle(),
-      false
-    );
-    beltShape.lineToPoint( wheel2Center.plus( wheel2CenterToArcStart ) );
-    beltShape.arcPoint(
-      wheel2Center,
-      wheel2Radius,
-      wheel2CenterToArcStart.angle(),
-      wheel2CenterToArcEnd.angle()
-    );
-    beltShape.lineToPoint( wheel1Center.plus( wheel1CenterToArcStart ) );
-    beltShape.close();
+      // vectors needed to create the belt shape
+      const wheel1CenterToWheelTwoCenter = wheel2Center.minus( wheel1Center );
+      const wheel1CenterToArcEnd =
+        wheel1CenterToWheelTwoCenter.perpendicular().withMagnitude( wheel1Radius ).rotated( -contactAngle );
+      const wheel1CenterToArcStart = wheel1CenterToArcEnd.rotated( Math.PI + 2 * contactAngle );
+      const wheel2CenterToArcStart =
+        wheel1CenterToWheelTwoCenter.perpendicular().withMagnitude( wheel2Radius ).rotated( -contactAngle );
+      const wheel2CenterToArcEnd = wheel2CenterToArcStart.rotated( Math.PI + 2 * contactAngle );
 
-    // @public (read-only) {Shape}
-    this.beltShape = beltShape;
+      // create the shape of the belt
+      const beltShape = new Shape();
+      beltShape.moveToPoint( wheel1Center.plus( wheel1CenterToArcStart ) );
+      beltShape.arcPoint(
+        wheel1Center,
+        wheel1Radius,
+        wheel1CenterToArcStart.angle(),
+        wheel1CenterToArcEnd.angle(),
+        false
+      );
+      beltShape.lineToPoint( wheel2Center.plus( wheel2CenterToArcStart ) );
+      beltShape.arcPoint(
+        wheel2Center,
+        wheel2Radius,
+        wheel2CenterToArcStart.angle(),
+        wheel2CenterToArcEnd.angle()
+      );
+      beltShape.lineToPoint( wheel1Center.plus( wheel1CenterToArcStart ) );
+      beltShape.close();
 
-    // @public {BooleanProperty}
-    this.isVisibleProperty = new Property( false );
+      // @public (read-only) {Shape}
+      this.beltShape = beltShape;
+
+      // @public {BooleanProperty}
+      this.isVisibleProperty = new Property( false );
+    }
   }
 
-  energyFormsAndChanges.register( 'Belt', Belt );
-
-  return inherit( Object, Belt );
+  return energyFormsAndChanges.register( 'Belt', Belt );
 } );
