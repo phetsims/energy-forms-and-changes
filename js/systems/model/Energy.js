88afc6d5aef3f3b972442d6d8b071c26689b57e4
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-06 16:22:38 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/model/Energy.js
+++ js/systems/model/Energy.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * a convenience type that collects together several things often needed about a unit of energy that is being produced
@@ -7,38 +7,38 @@
  * @author  John Blanco
  * @author  Andrew Adare
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
 
-  /**
-   * @param {EnergyType} type - energy type
-   * @param {number} amount - amount of energy, in joules
-   * @param {number} direction - direction of energy, in radians.  Not meaningful for all energy types.  Zero indicates
-   * to the right, PI/2 is up, and so forth.
-   */
-  function Energy( type, amount, direction, options ) {
+  class Energy {
 
-    options = _.extend( {
-      creationTime: null
-    }, options );
+    /**
+     * @param {EnergyType} type - energy type
+     * @param {number} amount - amount of energy, in joules
+     * @param {number} direction - direction of energy, in radians.  Not meaningful for all energy types.  Zero indicates
+     * to the right, PI/2 is up, and so forth.
+     */
+    constructor( type, amount, direction, options ) {
 
-    // @public (read-only) {EnergyType}
-    this.type = type;
+      options = _.extend( {
+        creationTime: null
+      }, options );
 
-    // @public (read-only) {number}
-    this.amount = amount;
+      // @public (read-only) {EnergyType}
+      this.type = type;
 
-    // @public (read-only) {number}
-    this.direction = direction;
+      // @public (read-only) {number}
+      this.amount = amount;
 
-    // @public (read-only) {number}
-    this.creationTime = options.creationTime;
+      // @public (read-only) {number}
+      this.direction = direction;
+
+      // @public (read-only) {number}
+      this.creationTime = options.creationTime;
+    }
   }
 
-  energyFormsAndChanges.register( 'Energy', Energy );
-
-  return inherit( Object, Energy );
+  return energyFormsAndChanges.register( 'Energy', Energy );
 } );
