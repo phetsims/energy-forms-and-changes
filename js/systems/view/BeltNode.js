c93d7bfb90b9bfbb808b9b9a60ae066894cc16db
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 10:34:31 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/view/BeltNode.js
+++ js/systems/view/BeltNode.js
@@ -1,40 +1,39 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * a Scenery Node representing a belt that connects two circular items, like a fan belt in an automobile
  *
  * @author John Blanco
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Path = require( 'SCENERY/nodes/Path' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Path = require( 'SCENERY/nodes/Path' );
 
-  /**
-   * @param {Belt} belt
-   * @param {ModelViewTransform2} modelViewTransform
-   * @param {Object} [options]
-   */
-  function BeltNode( belt, modelViewTransform, options ) {
+  class BeltNode extends Path {
 
-    options = _.extend( {
-      stroke: 'black',
-      lineWidth: 4
-    }, options );
+    /**
+     * @param {Belt} belt
+     * @param {ModelViewTransform2} modelViewTransform
+     * @param {Object} [options]
+     */
+    constructor( belt, modelViewTransform, options ) {
 
-    Path.call( this, modelViewTransform.modelToViewShape( belt.beltShape ), options );
+      options = _.extend( {
+        stroke: 'black',
+        lineWidth: 4
+      }, options );
 
-    // control visibility of the belt
-    var self = this;
-    belt.isVisibleProperty.link( function( isVisible ) {
-      self.setVisible( isVisible );
-    } );
+      super( modelViewTransform.modelToViewShape( belt.beltShape ), options );
+
+      // control visibility of the belt
+      belt.isVisibleProperty.link( isVisible => {
+        this.setVisible( isVisible );
+      } );
+    }
   }
 
-  energyFormsAndChanges.register( 'BeltNode', BeltNode );
-
-  return inherit( Path, BeltNode );
+  return energyFormsAndChanges.register( 'BeltNode', BeltNode );
 } );
