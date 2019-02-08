767d32badcc419836dd48a1e56b8a24478e3da5d
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-07 18:29:05 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/model/EnergyConverter.js
+++ js/systems/model/EnergyConverter.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * base type for energy converters, i.e. model elements that take energy from a source and convert it to something else
@@ -7,27 +7,23 @@
  * @author John Blanco
  * @author Andrew Adare
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var EnergySystemElement = require( 'ENERGY_FORMS_AND_CHANGES/systems/model/EnergySystemElement' );
-  var inherit = require( 'PHET_CORE/inherit' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const EnergySystemElement = require( 'ENERGY_FORMS_AND_CHANGES/systems/model/EnergySystemElement' );
 
-  /**
-   * @param {Image} iconImage Image to identify source on carousel menu
-   * @constructor
-   */
-  function EnergyConverter( iconImage ) {
-    this.incomingEnergyChunks = [];
-    this.outgoingEnergyChunks = [];
-    EnergySystemElement.call( this, iconImage );
-  }
+  class EnergyConverter extends EnergySystemElement {
 
-  energyFormsAndChanges.register( 'EnergyConverter', EnergyConverter );
-
-  return inherit( EnergySystemElement, EnergyConverter, {
+    /**
+     * @param {Image} iconImage Image to identify source on carousel menu
+     */
+    constructor( iconImage ) {
+      super( iconImage );
+      this.incomingEnergyChunks = [];
+      this.outgoingEnergyChunks = [];
+    }
 
     /**
      * get the energy chunks that this source wants to transfer to the next energy system element, reading clears the
@@ -35,10 +31,10 @@ define( function( require ) {
      * @returns {EnergyChunk[]}
      * @public
      */
-    extractOutgoingEnergyChunks: function() {
+    extractOutgoingEnergyChunks() {
       this.energyChunkList.removeAll( this.outgoingEnergyChunks );
       return this.outgoingEnergyChunks.splice( 0 );
-    },
+    }
 
     /**
      * Inject a list of energy chunks into this energy system element.  Once injected, it is the system's responsibility
@@ -46,19 +42,21 @@ define( function( require ) {
      * @parameter {EnergyChunk[]} energyChunks
      * @public
      */
-    injectEnergyChunks: function( energyChunks ) {
+    injectEnergyChunks( energyChunks ) {
       this.incomingEnergyChunks = _.union( this.incomingEnergyChunks, energyChunks );
-    },
+    }
 
     /**
      * clear internal list of energy chunks and outgoing energy chunks
      * @public
      */
-    clearEnergyChunks: function() {
+    clearEnergyChunks() {
       EnergySystemElement.prototype.clearEnergyChunks.call( this );
       this.incomingEnergyChunks.length = 0;
       this.outgoingEnergyChunks.length = 0;
     }
-  } );
+  }
+
+  return energyFormsAndChanges.register( 'EnergyConverter', EnergyConverter );
 } );
 
