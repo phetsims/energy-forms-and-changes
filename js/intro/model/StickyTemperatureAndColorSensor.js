b2af296491c66290d0785a8575f4ae105f43af5a
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 15:34:51 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/intro/model/StickyTemperatureAndColorSensor.js
+++ js/intro/model/StickyTemperatureAndColorSensor.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * a temperature and color sensor that sticks to movable objects
@@ -6,72 +6,69 @@
  * @author John Blanco
  * @author Jesse Greenberg
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var ElementFollower = require( 'ENERGY_FORMS_AND_CHANGES/common/model/ElementFollower' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var TemperatureAndColorSensor = require( 'ENERGY_FORMS_AND_CHANGES/common/model/TemperatureAndColorSensor' );
+  const ElementFollower = require( 'ENERGY_FORMS_AND_CHANGES/common/model/ElementFollower' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const TemperatureAndColorSensor = require( 'ENERGY_FORMS_AND_CHANGES/common/model/TemperatureAndColorSensor' );
 
-  /**
-   * @param {EFACIntroModel} model
-   * @param {Vector2} initialPosition
-   * @param {boolean} initiallyActive
-   * @constructor
-   */
-  function StickyTemperatureAndColorSensor( model, initialPosition, initiallyActive ) {
+  class StickyTemperatureAndColorSensor extends TemperatureAndColorSensor {
 
-    var self = this;
-    TemperatureAndColorSensor.call( this, model, initialPosition, initiallyActive );
+    /**
+     * @param {EFACIntroModel} model
+     * @param {Vector2} initialPosition
+     * @param {boolean} initiallyActive
+     * @constructor
+     */
+    constructor( model, initialPosition, initiallyActive ) {
+      super( model, initialPosition, initiallyActive );
 
-    // @private
-    this.elementFollower = new ElementFollower( this.positionProperty );
+      // @private
+      this.elementFollower = new ElementFollower( this.positionProperty );
 
-    // Monitor the state of the 'userControlled' property in order to detect when the user drops this thermometer and
-    // determine whether or not it was dropped over anything to which it should stick.
-    this.userControlledProperty.link( function( userControlled ) {
+      // Monitor the state of the 'userControlled' property in order to detect when the user drops this thermometer and
+      // determine whether or not it was dropped over anything to which it should stick.
+      this.userControlledProperty.link( userControlled => {
 
-      // if being dragged, stop following any objects
-      if ( userControlled ) {
-        self.elementFollower.stopFollowing();
-      }
+        // if being dragged, stop following any objects
+        if ( userControlled ) {
+          this.elementFollower.stopFollowing();
+        }
 
-      // if the thermometer was dropped, see if it was dropped over something that it should follow
-      else {
-        model.blocks.forEach( function( block ) {
-          if ( block.getProjectedShape().containsPoint( self.positionProperty.value ) ) {
+        // if the thermometer was dropped, see if it was dropped over something that it should follow
+        else {
+          model.blocks.forEach( block => {
+            if ( block.getProjectedShape().containsPoint( this.positionProperty.value ) ) {
 
-            // stick to this block
-            self.elementFollower.startFollowing( block.positionProperty );
-          }
-        } );
-
-        if ( !self.elementFollower.isFollowing() ) {
-          model.beakers.forEach( function( beaker ) {
-            if ( beaker.thermalContactArea.containsPoint( self.positionProperty.value ) ) {
-
-              // stick to this beaker
-              self.elementFollower.startFollowing( beaker.positionProperty );
+              // stick to this block
+              this.elementFollower.startFollowing( block.positionProperty );
             }
           } );
+
+          if ( !this.elementFollower.isFollowing() ) {
+            model.beakers.forEach( beaker => {
+              if ( beaker.thermalContactArea.containsPoint( this.positionProperty.value ) ) {
+
+                // stick to this beaker
+                this.elementFollower.startFollowing( beaker.positionProperty );
+              }
+            } );
+          }
         }
-      }
-    } );
-  }
-
-  energyFormsAndChanges.register( 'StickyTemperatureAndColorSensor', StickyTemperatureAndColorSensor );
-
-  return inherit( TemperatureAndColorSensor, StickyTemperatureAndColorSensor, {
+      } );
+    }
 
     /**
      * restore initial state
      * @public
      */
-    reset: function() {
+    reset() {
       this.elementFollower.stopFollowing();
       TemperatureAndColorSensor.prototype.reset.call( this );
     }
-  } );
+  }
+
+  return energyFormsAndChanges.register( 'StickyTemperatureAndColorSensor', StickyTemperatureAndColorSensor );
 } );
