ecfed64a920caf7957ddbde97e9fd387a01eed39
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 15:28:12 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/intro/model/SimSpeed.js
+++ js/intro/model/SimSpeed.js
@@ -1,18 +1,18 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * enum for sim speed settings
  *
  * @author John Blanco
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
 
   // @public
-  var SimSpeed = {
+  const SimSpeed = {
     NORMAL: 'NORMAL',
     FAST_FORWARD: 'FAST_FORWARD'
   };
@@ -20,7 +20,5 @@ define( function( require ) {
   // verify that enum is immutable, without the runtime penalty in production code
   if ( assert ) { Object.freeze( SimSpeed ); }
 
-  energyFormsAndChanges.register( 'SimSpeed', SimSpeed );
-
-  return SimSpeed;
+  return energyFormsAndChanges.register( 'SimSpeed', SimSpeed );
 } );
