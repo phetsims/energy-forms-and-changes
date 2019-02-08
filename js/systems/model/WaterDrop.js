c7594af94a1681b9e80449a1d928a895d3c40ae8
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-07 17:38:53 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/model/WaterDrop.js
+++ js/systems/model/WaterDrop.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * a model of a drop of water, generally used to create a stream of water coming from, say, a faucet
@@ -7,45 +7,43 @@
  * @author Andrew Adare
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var Dimension2 = require( 'DOT/Dimension2' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Property = require( 'AXON/Property' );
+  const Dimension2 = require( 'DOT/Dimension2' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Property = require( 'AXON/Property' );
 
   // the following constant is used to adjust the way in which the drop elongates as its velocity increases
-  var WIDTH_CHANGE_TWEAK_FACTOR = 0.05;
+  const WIDTH_CHANGE_TWEAK_FACTOR = 0.05;
 
-  /**
-   * @param {Vector2} initialPosition - (x,y) position in model space
-   * @param {Vector2} initialVelocity - 2D velocity at initialization
-   * @param {Dimension2} size - droplet dimensions
-   */
-  function WaterDrop( initialPosition, initialVelocity, size ) {
+  class WaterDrop {
 
-    var self = this;
+    /**
+     * @param {Vector2} initialPosition - (x,y) position in model space
+     * @param {Vector2} initialVelocity - 2D velocity at initialization
+     * @param {Dimension2} size - droplet dimensions
+     */
+    constructor( initialPosition, initialVelocity, size ) {
 
-    // @public {Vector2} - after being transformed to view coordinates, this position is the distance from the faucet head
-    this.position = initialPosition;
+      // @public {Vector2} - after being transformed to view coordinates, this position is the distance from the faucet head
+      this.position = initialPosition;
 
-    // @public {Property.<Vector2>}
-    this.velocityProperty = new Property( initialVelocity );
+      // @public {Property.<Vector2>}
+      this.velocityProperty = new Property( initialVelocity );
 
-    // @public (read-only) {Dimension2}
-    this.size = size;
+      // @public (read-only) {Dimension2}
+      this.size = size;
 
-    // adjust the size as the velocity changes, mimicking how water drops thin out as they fall through air
-    this.velocityProperty.link( function( velocity ) {
-      var newWidth = ( 1 / ( 1 + velocity.magnitude() * WIDTH_CHANGE_TWEAK_FACTOR ) ) * self.size.width;
-      var newHeight = ( self.size.height * self.size.width ) / newWidth;
-      self.size.set( new Dimension2( newWidth, newHeight ) );
-    } );
+      // adjust the size as the velocity changes, mimicking how water drops thin out as they fall through air
+      this.velocityProperty.link( velocity => {
+        const newWidth = ( 1 / ( 1 + velocity.magnitude() * WIDTH_CHANGE_TWEAK_FACTOR ) ) * this.size.width;
+        const newHeight = ( this.size.height * this.size.width ) / newWidth;
+        this.size.set( new Dimension2( newWidth, newHeight ) );
+      } );
+    }
   }
 
-  energyFormsAndChanges.register( 'WaterDrop', WaterDrop );
-
-  return inherit( Object, WaterDrop );
+  return energyFormsAndChanges.register( 'WaterDrop', WaterDrop );
 } );
