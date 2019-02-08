c002b12f578682fc5c71e1172dc1dad5a5ba58cf
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 13:00:36 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/intro/view/SimSpeedButtonGroup.js
+++ js/intro/view/SimSpeedButtonGroup.js
@@ -5,54 +5,53 @@
  *
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var AquaRadioButton = require( 'SUN/AquaRadioButton' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var SimSpeed = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/SimSpeed' );
-  var Text = require( 'SCENERY/nodes/Text' );
+  const AquaRadioButton = require( 'SUN/AquaRadioButton' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const SimSpeed = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/SimSpeed' );
+  const Text = require( 'SCENERY/nodes/Text' );
 
   // strings
-  var fastForwardString = require( 'string!ENERGY_FORMS_AND_CHANGES/fastForward' );
-  var normalString = require( 'string!ENERGY_FORMS_AND_CHANGES/normal' );
+  const fastForwardString = require( 'string!ENERGY_FORMS_AND_CHANGES/fastForward' );
+  const normalString = require( 'string!ENERGY_FORMS_AND_CHANGES/normal' );
 
   // static data
-  var RADIO_BUTTON_FONT = new PhetFont( 16 );
-  var SPEED_BUTTON_MAX_WIDTH = 200; // empirically determined
+  const RADIO_BUTTON_FONT = new PhetFont( 16 );
+  const SPEED_BUTTON_MAX_WIDTH = 200; // empirically determined
 
-  /**
-   * @param {Property.<SimSpeed>} simSpeedProperty
-   * @constructor
-   */
-  function SimSpeedButtonGroup( simSpeedProperty ) {
+  class SimSpeedButtonGroup extends HBox {
 
-    // create the text nodes for normal and fast forward buttons
-    var normalText = new Text( normalString, {
-      font: RADIO_BUTTON_FONT,
-      maxWidth: SPEED_BUTTON_MAX_WIDTH
-    } );
-    var fastForwardText = new Text( fastForwardString, {
-      font: RADIO_BUTTON_FONT,
-      maxWidth: SPEED_BUTTON_MAX_WIDTH
-    } );
+    /**
+     * @param {Property.<SimSpeed>} simSpeedProperty
+     */
+    constructor( simSpeedProperty ) {
 
-    // create the normal and fast forward radio buttons
-    var normalButton = new AquaRadioButton( simSpeedProperty, SimSpeed.NORMAL, normalText );
-    var fastForwardButton = new AquaRadioButton( simSpeedProperty, SimSpeed.FAST_FORWARD, fastForwardText );
+      // create the text nodes for normal and fast forward buttons
+      const normalText = new Text( normalString, {
+        font: RADIO_BUTTON_FONT,
+        maxWidth: SPEED_BUTTON_MAX_WIDTH
+      } );
+      const fastForwardText = new Text( fastForwardString, {
+        font: RADIO_BUTTON_FONT,
+        maxWidth: SPEED_BUTTON_MAX_WIDTH
+      } );
 
-    HBox.call( this, {
-      children: [ normalButton, fastForwardButton ],
-      spacing: 10
-    } );
+      // create the normal and fast forward radio buttons
+      const normalButton = new AquaRadioButton( simSpeedProperty, SimSpeed.NORMAL, normalText );
+      const fastForwardButton = new AquaRadioButton( simSpeedProperty, SimSpeed.FAST_FORWARD, fastForwardText );
+
+      super( {
+        children: [ normalButton, fastForwardButton ],
+        spacing: 10
+      } );
+    }
   }
 
-  energyFormsAndChanges.register( 'SimSpeedButtonGroup', SimSpeedButtonGroup );
-
-  return inherit( HBox, SimSpeedButtonGroup );
+  return energyFormsAndChanges.register( 'SimSpeedButtonGroup', SimSpeedButtonGroup );
 } );
 
