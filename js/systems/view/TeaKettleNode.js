a601a69aff94d4b4dac473b44c292c83c2118155
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 11:42:28 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/view/TeaKettleNode.js
+++ js/systems/view/TeaKettleNode.js
@@ -1,152 +1,150 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * a Scenery Node that depicts a tea kettle on a burner
  *
  * @author John Blanco
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var Bounds2 = require( 'DOT/Bounds2' );
-  var BurnerStandNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/BurnerStandNode' );
-  var Color = require( 'SCENERY/util/Color' );
-  var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
-  var EnergyChunkLayer = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkLayer' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var HeaterCoolerBack = require( 'SCENERY_PHET/HeaterCoolerBack' );
-  var HeaterCoolerFront = require( 'SCENERY_PHET/HeaterCoolerFront' );
-  var Image = require( 'SCENERY/nodes/Image' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var LinearGradient = require( 'SCENERY/util/LinearGradient' );
-  var MoveFadeModelElementNode = require( 'ENERGY_FORMS_AND_CHANGES/systems/view/MoveFadeModelElementNode' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var NumberProperty = require( 'AXON/NumberProperty' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var Shape = require( 'KITE/Shape' );
-  var Vector2 = require( 'DOT/Vector2' );
+  const Bounds2 = require( 'DOT/Bounds2' );
+  const BurnerStandNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/BurnerStandNode' );
+  const Color = require( 'SCENERY/util/Color' );
+  const EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
+  const EnergyChunkLayer = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkLayer' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const HeaterCoolerBack = require( 'SCENERY_PHET/HeaterCoolerBack' );
+  const HeaterCoolerFront = require( 'SCENERY_PHET/HeaterCoolerFront' );
+  const Image = require( 'SCENERY/nodes/Image' );
+  const LinearGradient = require( 'SCENERY/util/LinearGradient' );
+  const MoveFadeModelElementNode = require( 'ENERGY_FORMS_AND_CHANGES/systems/view/MoveFadeModelElementNode' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const NumberProperty = require( 'AXON/NumberProperty' );
+  const Path = require( 'SCENERY/nodes/Path' );
+  const Shape = require( 'KITE/Shape' );
+  const Vector2 = require( 'DOT/Vector2' );
 
   // images
-  var teapotImage = require( 'image!ENERGY_FORMS_AND_CHANGES/tea_kettle_large.png' );
+  const teapotImage = require( 'image!ENERGY_FORMS_AND_CHANGES/tea_kettle_large.png' );
 
   // constants
-  var BURNER_MODEL_BOUNDS = new Bounds2( -0.037, -0.0075, 0.037, 0.0525 ); // in meters
-  var BURNER_EDGE_TO_HEIGHT_RATIO = 0.2; // multiplier empirically determined for best look
-  var MAX_HEIGHT_AND_WIDTH = 200; // for tea kettle steam node
-  var HEATER_COOLER_NODE_SCALE = 0.85; // empirically determined for best look
+  const BURNER_MODEL_BOUNDS = new Bounds2( -0.037, -0.0075, 0.037, 0.0525 ); // in meters
+  const BURNER_EDGE_TO_HEIGHT_RATIO = 0.2; // multiplier empirically determined for best look
+  const MAX_HEIGHT_AND_WIDTH = 200; // for tea kettle steam node
+  const HEATER_COOLER_NODE_SCALE = 0.85; // empirically determined for best look
 
-  /**
-   * @param {TeaKettle} teaKettle
-   * @param {Property.<boolean>} energyChunksVisibleProperty
-   * @param {ModelViewTransform2} modelViewTransform
-   * @constructor
-   */
-  function TeaKettleNode( teaKettle, energyChunksVisibleProperty, modelViewTransform ) {
+  class TeaKettleNode extends MoveFadeModelElementNode {
 
-    var self = this;
-    MoveFadeModelElementNode.call( this, teaKettle, modelViewTransform );
+    /**
+     * @param {TeaKettle} teaKettle
+     * @param {Property.<boolean>} energyChunksVisibleProperty
+     * @param {ModelViewTransform2} modelViewTransform
+     */
+    constructor( teaKettle, energyChunksVisibleProperty, modelViewTransform ) {
+      super( teaKettle, modelViewTransform );
 
-    var teaKettleImageNode = new Image( teapotImage, { right: 114, bottom: 53 } );
+      const teaKettleImageNode = new Image( teapotImage, { right: 114, bottom: 53 } );
 
-    // create a mapping between the slider position and the steam proportion, which prevents very small values
-    this.heaterSettingProperty = new NumberProperty( 0 );
-    this.heaterSettingProperty.link( function( setting ) {
-      var mappedSetting = setting === 0 ? 0 : 0.25 + ( setting * 0.75 );
-      assert && assert( mappedSetting >= 0 && mappedSetting <= 1 );
-      teaKettle.heatCoolAmountProperty.set( mappedSetting );
-    } );
+      // create a mapping between the slider position and the steam proportion, which prevents very small values
+      this.heaterSettingProperty = new NumberProperty( 0 );
+      this.heaterSettingProperty.link( setting => {
+        const mappedSetting = setting === 0 ? 0 : 0.25 + ( setting * 0.75 );
+        assert && assert( mappedSetting >= 0 && mappedSetting <= 1 );
+        teaKettle.heatCoolAmountProperty.set( mappedSetting );
+      } );
 
-    // node for heater-cooler bucket - front and back are added separately to support layering of energy chunks
-    var heaterCoolerBack = new HeaterCoolerBack( this.heaterSettingProperty, { scale: HEATER_COOLER_NODE_SCALE } );
-    var heaterCoolerFront = new HeaterCoolerFront( this.heaterSettingProperty, {
-      snapToZero: false,
-      coolEnabled: false,
-      scale: HEATER_COOLER_NODE_SCALE
-    } );
+      // node for heater-cooler bucket - front and back are added separately to support layering of energy chunks
+      const heaterCoolerBack = new HeaterCoolerBack( this.heaterSettingProperty, { scale: HEATER_COOLER_NODE_SCALE } );
+      const heaterCoolerFront = new HeaterCoolerFront( this.heaterSettingProperty, {
+        snapToZero: false,
+        coolEnabled: false,
+        scale: HEATER_COOLER_NODE_SCALE
+      } );
 
-    // burner stand node
-    var burnerSize = modelViewTransform.modelToViewShape( BURNER_MODEL_BOUNDS );
-    var burnerProjection = burnerSize.width * BURNER_EDGE_TO_HEIGHT_RATIO;
-    var burnerStandNode = new BurnerStandNode( burnerSize, burnerProjection );
+      // burner stand node
+      const burnerSize = modelViewTransform.modelToViewShape( BURNER_MODEL_BOUNDS );
+      const burnerProjection = burnerSize.width * BURNER_EDGE_TO_HEIGHT_RATIO;
+      const burnerStandNode = new BurnerStandNode( burnerSize, burnerProjection );
 
-    burnerStandNode.centerTop = teaKettleImageNode.centerBottom.plus( new Vector2( 0, -teaKettleImageNode.height / 4 ) );
-    heaterCoolerBack.centerX = burnerStandNode.centerX;
-    heaterCoolerBack.bottom = burnerStandNode.bottom - burnerProjection / 2;
-    heaterCoolerFront.leftTop = heaterCoolerBack.getHeaterFrontPosition();
+      burnerStandNode.centerTop = teaKettleImageNode.centerBottom.plus( new Vector2( 0, -teaKettleImageNode.height / 4 ) );
+      heaterCoolerBack.centerX = burnerStandNode.centerX;
+      heaterCoolerBack.bottom = burnerStandNode.bottom - burnerProjection / 2;
+      heaterCoolerFront.leftTop = heaterCoolerBack.getHeaterFrontPosition();
 
-    // make the tea kettle & stand transparent when the energy chunks are visible
-    energyChunksVisibleProperty.link( function( chunksVisible ) {
-      var opacity = chunksVisible ? 0.7 : 1;
-      teaKettleImageNode.setOpacity( opacity );
-      burnerStandNode.setOpacity( opacity );
-    } );
+      // make the tea kettle & stand transparent when the energy chunks are visible
+      energyChunksVisibleProperty.link( chunksVisible => {
+        const opacity = chunksVisible ? 0.7 : 1;
+        teaKettleImageNode.setOpacity( opacity );
+        burnerStandNode.setOpacity( opacity );
+      } );
 
-    var energyChunkLayer = new EnergyChunkLayer(
-      teaKettle.energyChunkList,
-      modelViewTransform,
-      { parentPositionProperty: teaKettle.positionProperty }
-    );
+      const energyChunkLayer = new EnergyChunkLayer(
+        teaKettle.energyChunkList,
+        modelViewTransform,
+        { parentPositionProperty: teaKettle.positionProperty }
+      );
 
-    var spoutXY = new Vector2( teaKettleImageNode.bounds.maxX - 5, teaKettleImageNode.bounds.minY + 16 );
-    this.steamNode = new SteamNode(
-      spoutXY,
-      teaKettle.energyProductionRateProperty,
-      EFACConstants.MAX_ENERGY_PRODUCTION_RATE,
-      teaKettle.activeProperty
-    );
+      const spoutXY = new Vector2( teaKettleImageNode.bounds.maxX - 5, teaKettleImageNode.bounds.minY + 16 );
+      this.steamNode = new SteamNode(
+        spoutXY,
+        teaKettle.energyProductionRateProperty,
+        EFACConstants.MAX_ENERGY_PRODUCTION_RATE,
+        teaKettle.activeProperty
+      );
 
-    this.addChild( heaterCoolerBack );
-    this.addChild( energyChunkLayer );
-    this.addChild( heaterCoolerFront );
-    this.addChild( burnerStandNode );
-    this.addChild( teaKettleImageNode );
-    this.addChild( this.steamNode );
+      this.addChild( heaterCoolerBack );
+      this.addChild( energyChunkLayer );
+      this.addChild( heaterCoolerFront );
+      this.addChild( burnerStandNode );
+      this.addChild( teaKettleImageNode );
+      this.addChild( this.steamNode );
 
-    // reset the heater slider when the tea kettle is deactivated
-    teaKettle.activeProperty.link( function( active ) {
-      if ( !active ) {
-        self.heaterSettingProperty.reset();
-      }
-    } );
+      // reset the heater slider when the tea kettle is deactivated
+      teaKettle.activeProperty.link( active => {
+        if ( !active ) {
+          this.heaterSettingProperty.reset();
+        }
+      } );
+    }
   }
 
-  /**
-   * inner type for depicting steam that emits from the tea kettle when it's hot
-   * @param {Vector2} origin
-   * @param {NumberProperty} energyOutputProperty
-   * @param {number} maxEnergyOutput
-   * @param {BooleanProperty} activeProperty
-   * @constructor
-   */
-  function SteamNode( origin, energyOutputProperty, maxEnergyOutput, activeProperty ) {
+  class SteamNode extends Node {
 
-    Node.call( this );
+    /**
+     * inner type for depicting steam that emits from the tea kettle when it's hot
+     * @param {Vector2} origin
+     * @param {NumberProperty} energyOutputProperty
+     * @param {number} maxEnergyOutput
+     * @param {BooleanProperty} activeProperty
+     * @constructor
+     */
+    constructor( origin, energyOutputProperty, maxEnergyOutput, activeProperty ) {
+      super();
 
-    this.origin = origin;
-    this.energyOutputProperty = energyOutputProperty;
-    this.maxEnergyOutput = maxEnergyOutput;
-    this.activeProperty = activeProperty;
+      this.origin = origin;
+      this.energyOutputProperty = energyOutputProperty;
+      this.maxEnergyOutput = maxEnergyOutput;
+      this.activeProperty = activeProperty;
 
-    // create paths from shapes
-    this.stemPath = new Path( null );
+      // create paths from shapes
+      this.stemPath = new Path( null );
 
-    // create paths from shapes
-    this.bodyPath = new Path( null, {
-      fill: Color.WHITE,
-      lineWidth: 1,
-      stroke: Color.WHITE,
-      opacity: 0.5
-    } );
+      // create paths from shapes
+      this.bodyPath = new Path( null, {
+        fill: Color.WHITE,
+        lineWidth: 1,
+        stroke: Color.WHITE,
+        opacity: 0.5
+      } );
 
-    this.stemPath.setTranslation( this.origin );
-    this.bodyPath.setTranslation( this.origin );
+      this.stemPath.setTranslation( this.origin );
+      this.bodyPath.setTranslation( this.origin );
 
-    this.addChild( this.stemPath );
-    this.addChild( this.bodyPath );
-  }
-
-  inherit( Node, SteamNode, {
+      this.addChild( this.stemPath );
+      this.addChild( this.bodyPath );
+    }
 
     /**
      * step the steam node forward in time
@@ -154,25 +152,25 @@ define( function( require ) {
      * @public
      */
     //TODO: This animation should use dt so that it looks consistent across all platforms
-    step: function( dt ) {
+    step( dt ) {
       if ( this.activeProperty.get() ) {
 
-        var proportion = this.energyOutputProperty.get() / this.maxEnergyOutput;
-        var heightAndWidth = proportion * MAX_HEIGHT_AND_WIDTH;
+        const proportion = this.energyOutputProperty.get() / this.maxEnergyOutput;
+        const heightAndWidth = proportion * MAX_HEIGHT_AND_WIDTH;
 
-        var stemBaseWidth = 8; // Empirically chosen
+        const stemBaseWidth = 8; // Empirically chosen
 
         // add points to steam cloud outline array
-        var cloudStem = new Shape();
-        var cloudBody = new Shape();
+        const cloudStem = new Shape();
+        const cloudBody = new Shape();
 
         // cloud stem
-        var startPoint = new Vector2( -stemBaseWidth / 2, 0 ).rotated( Math.PI / 4 );
+        const startPoint = new Vector2( -stemBaseWidth / 2, 0 ).rotated( Math.PI / 4 );
 
         // opening angle of steam stream (/2)
-        var stemHalfAngle = 0.5 * Math.PI / 4 * ( 1 + 0.3 * ( phet.joist.random.nextDouble() - 0.5 ) );
+        const stemHalfAngle = 0.5 * Math.PI / 4 * ( 1 + 0.3 * ( phet.joist.random.nextDouble() - 0.5 ) );
 
-        var stemEdge = new Vector2( heightAndWidth / 4, -heightAndWidth / 4 );
+        const stemEdge = new Vector2( heightAndWidth / 4, -heightAndWidth / 4 );
 
         cloudStem.moveToPoint( startPoint );
 
@@ -185,12 +183,12 @@ define( function( require ) {
         cloudStem.lineToPoint( startPoint );
 
         // cloud body
-        var cloudSize = heightAndWidth * 0.9; // Empirically chosen
-        var cloudCenter = new Vector2( heightAndWidth - cloudSize / 2, -cloudSize / 2 );
-        var nPoints = 16;
-        for ( var i = 0; i < nPoints; i++ ) {
-          var radiusVector = new Vector2( cloudSize / 2 * ( 1 + 0.1 * ( phet.joist.random.nextDouble() - 0.5 ) ), 0 );
-          var point = cloudCenter.plus( radiusVector.rotated( i * Math.PI * 2 / nPoints ) );
+        const cloudSize = heightAndWidth * 0.9; // Empirically chosen
+        const cloudCenter = new Vector2( heightAndWidth - cloudSize / 2, -cloudSize / 2 );
+        const nPoints = 16;
+        for ( let i = 0; i < nPoints; i++ ) {
+          const radiusVector = new Vector2( cloudSize / 2 * ( 1 + 0.1 * ( phet.joist.random.nextDouble() - 0.5 ) ), 0 );
+          const point = cloudCenter.plus( radiusVector.rotated( i * Math.PI * 2 / nPoints ) );
           i ? cloudBody.lineToPoint( point ) : cloudBody.moveToPoint( point );
         }
 
@@ -210,10 +208,8 @@ define( function( require ) {
         this.bodyPath.setVisible( false );
       }
     }
-  } );
+  }
 
-  energyFormsAndChanges.register( 'TeaKettleNode', TeaKettleNode );
-
-  return inherit( MoveFadeModelElementNode, TeaKettleNode );
+  return energyFormsAndChanges.register( 'TeaKettleNode', TeaKettleNode );
 } );
 
