15df52214680e446659171b307636dafa25b6b36
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 11:51:55 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/view/MoveFadeModelElementNode.js
+++ js/systems/view/MoveFadeModelElementNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * base type for a Scenery Node that moves as the associated model element moves and and fades in and out as the opacity
@@ -7,42 +7,39 @@
  * @author John Blanco
  * @author Andrew Adare
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Node = require( 'SCENERY/nodes/Node' );
 
-  /**
-   * @param {PositionableFadableModelElement} modelElement
-   * @param {ModelViewTransform} modelViewTransform
-   * @constructor
-   */
-  function MoveFadeModelElementNode( modelElement, modelViewTransform ) {
-    Node.call( this );
+  class MoveFadeModelElementNode extends Node {
 
-    var self = this;
+    /**
+     * @param {PositionableFadableModelElement} modelElement
+     * @param {ModelViewTransform} modelViewTransform
+     */
+    constructor( modelElement, modelViewTransform ) {
+      super();
 
-    // update our position as the model element moves
-    modelElement.positionProperty.link( function( offset ) {
-      self.setTranslation( modelViewTransform.modelToViewPosition( offset ) );
-    } );
+      // update our position as the model element moves
+      modelElement.positionProperty.link( offset => {
+        this.setTranslation( modelViewTransform.modelToViewPosition( offset ) );
+      } );
 
-    // TODO: The model shouldn't have opacity, it should have something like a selection proportion.
-    // update our opacity as the model element fades in and out
-    modelElement.opacityProperty.link( function( opacity ) {
-      self.setOpacity( opacity );
-      if ( opacity === 0 && self.visible ) {
-        self.visible = false;
-      }
-      else if ( opacity > 0 && !self.visible ) {
-        self.visible = true;
-      }
-    } );
+      // TODO: The model shouldn't have opacity, it should have something like a selection proportion.
+      // update our opacity as the model element fades in and out
+      modelElement.opacityProperty.link( opacity => {
+        this.setOpacity( opacity );
+        if ( opacity === 0 && this.visible ) {
+          this.visible = false;
+        }
+        else if ( opacity > 0 && !this.visible ) {
+          this.visible = true;
+        }
+      } );
+    }
   }
 
-  energyFormsAndChanges.register( 'MoveFadeModelElementNode', MoveFadeModelElementNode );
-
-  return inherit( Node, MoveFadeModelElementNode );
+  return energyFormsAndChanges.register( 'MoveFadeModelElementNode', MoveFadeModelElementNode );
 } );
