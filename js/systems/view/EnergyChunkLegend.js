6cfb3e9af7731ab538e05edac02ba00ad4ea60ee
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 12:09:32 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/systems/view/EnergyChunkLegend.js
+++ js/systems/view/EnergyChunkLegend.js
@@ -1,110 +1,108 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
- *
  * a Scenery Node that represent a legend that describes the different types of energy chunks
  *
  * @author John Blanco
  * @author Jesse Greenberg
  * @author Andrew Adare
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
-  var EnergyChunk = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyChunk' );
-  var EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var EnergyType = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyType' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Panel = require( 'SUN/Panel' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Property = require( 'AXON/Property' );
-  var Text = require( 'SCENERY/nodes/Text' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
-  var Vector2 = require( 'DOT/Vector2' );
+  const EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
+  const EnergyChunk = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyChunk' );
+  const EnergyChunkNode = require( 'ENERGY_FORMS_AND_CHANGES/common/view/EnergyChunkNode' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const EnergyType = require( 'ENERGY_FORMS_AND_CHANGES/common/model/EnergyType' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const Panel = require( 'SUN/Panel' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const Property = require( 'AXON/Property' );
+  const Text = require( 'SCENERY/nodes/Text' );
+  const VBox = require( 'SCENERY/nodes/VBox' );
+  const Vector2 = require( 'DOT/Vector2' );
 
   // strings
-  var chemicalString = require( 'string!ENERGY_FORMS_AND_CHANGES/chemical' );
-  var electricalString = require( 'string!ENERGY_FORMS_AND_CHANGES/electrical' );
-  var formsOfEnergyString = require( 'string!ENERGY_FORMS_AND_CHANGES/formsOfEnergy' );
-  var lightString = require( 'string!ENERGY_FORMS_AND_CHANGES/light' );
-  var mechanicalString = require( 'string!ENERGY_FORMS_AND_CHANGES/mechanical' );
-  var thermalString = require( 'string!ENERGY_FORMS_AND_CHANGES/thermal' );
+  const chemicalString = require( 'string!ENERGY_FORMS_AND_CHANGES/chemical' );
+  const electricalString = require( 'string!ENERGY_FORMS_AND_CHANGES/electrical' );
+  const formsOfEnergyString = require( 'string!ENERGY_FORMS_AND_CHANGES/formsOfEnergy' );
+  const lightString = require( 'string!ENERGY_FORMS_AND_CHANGES/light' );
+  const mechanicalString = require( 'string!ENERGY_FORMS_AND_CHANGES/mechanical' );
+  const thermalString = require( 'string!ENERGY_FORMS_AND_CHANGES/thermal' );
 
   // constants
-  var LEGEND_ENTRY_FONT = new PhetFont( 14 );
+  const LEGEND_ENTRY_FONT = new PhetFont( 14 );
 
-  /**
-   * @param {ModelViewTransform2} modelViewTransform
-   * @param {Object} [options]
-   * @constructor
-   */
-  function EnergyChunkLegend( modelViewTransform, options ) {
+  class EnergyChunkLegend extends Panel {
 
-    options = _.extend( {
-      fill: EFACConstants.CONTROL_PANEL_BACKGROUND_COLOR,
-      stroke: EFACConstants.CONTROL_PANEL_OUTLINE_STROKE,
-      lineWidth: EFACConstants.CONTROL_PANEL_OUTLINE_LINE_WIDTH,
-      cornerRadius: EFACConstants.ENERGY_SYMBOLS_PANEL_CORNER_RADIUS
-    }, options );
+    /**
+     * @param {ModelViewTransform2} modelViewTransform
+     * @param {Object} [options]
+     */
+    constructor( modelViewTransform, options ) {
 
-    // title that appears at the top of the legend
-    var titleText = new Text( formsOfEnergyString, {
-      font: new PhetFont( {
-        size: 14,
-        weight: 'bold'
-      } ),
-      maxWidth: 130
-    } );
+      options = _.extend( {
+        fill: EFACConstants.CONTROL_PANEL_BACKGROUND_COLOR,
+        stroke: EFACConstants.CONTROL_PANEL_OUTLINE_STROKE,
+        lineWidth: EFACConstants.CONTROL_PANEL_OUTLINE_LINE_WIDTH,
+        cornerRadius: EFACConstants.ENERGY_SYMBOLS_PANEL_CORNER_RADIUS
+      }, options );
 
-    // add an entry for each type of energy that can appear in the sim
-    var content = new VBox( {
-      children: [
-        titleText,
-        createEnergyChunkSymbol( mechanicalString, EnergyType.MECHANICAL, modelViewTransform ),
-        createEnergyChunkSymbol( electricalString, EnergyType.ELECTRICAL, modelViewTransform ),
-        createEnergyChunkSymbol( thermalString, EnergyType.THERMAL, modelViewTransform ),
-        createEnergyChunkSymbol( lightString, EnergyType.LIGHT, modelViewTransform ),
-        createEnergyChunkSymbol( chemicalString, EnergyType.CHEMICAL, modelViewTransform )
-      ],
-      align: 'left',
-      spacing: 6
-    } );
+      // title that appears at the top of the legend
+      const titleText = new Text( formsOfEnergyString, {
+        font: new PhetFont( {
+          size: 14,
+          weight: 'bold'
+        } ),
+        maxWidth: 130
+      } );
 
-    Panel.call( this, content, options );
+      // add an entry for each type of energy that can appear in the sim
+      const content = new VBox( {
+        children: [
+          titleText,
+          EnergyChunkLegend.createEnergyChunkSymbol( mechanicalString, EnergyType.MECHANICAL, modelViewTransform ),
+          EnergyChunkLegend.createEnergyChunkSymbol( electricalString, EnergyType.ELECTRICAL, modelViewTransform ),
+          EnergyChunkLegend.createEnergyChunkSymbol( thermalString, EnergyType.THERMAL, modelViewTransform ),
+          EnergyChunkLegend.createEnergyChunkSymbol( lightString, EnergyType.LIGHT, modelViewTransform ),
+          EnergyChunkLegend.createEnergyChunkSymbol( chemicalString, EnergyType.CHEMICAL, modelViewTransform )
+        ],
+        align: 'left',
+        spacing: 6
+      } );
+
+      super( content, options );
+    }
+
+    /**
+     * helper function to create energy chunk legend entries
+     * @param labelString - the label for this legend entry
+     * @param energyType - the type of energy for this legend entry
+     * @param modelViewTransform - needs to be passed in to create an EnergyChunk
+     * @public
+     */
+    static createEnergyChunkSymbol( labelString, energyType, modelViewTransform ) {
+      const labelText = new Text( labelString, {
+        font: LEGEND_ENTRY_FONT,
+        maxWidth: 100
+      } );
+
+      // The EnergyChunks that are created here are not going to be used in the simulation, they are only needed for the
+      // EnergyChunkNodes that are displayed in the legend.
+      const iconNode = new EnergyChunkNode(
+        new EnergyChunk( energyType, Vector2.ZERO, Vector2.ZERO, new Property( true ) ),
+        modelViewTransform
+      );
+
+      return new HBox( {
+        children: [ iconNode, labelText ],
+        spacing: 10
+      } );
+    }
   }
 
-  /**
-   * helper function to create energy chunk legend entries
-   * @param labelString - the label for this legend entry
-   * @param energyType - the type of energy for this legend entry
-   * @param modelViewTransform - needs to be passed in to create an EnergyChunk
-   * @public
-   */
-  function createEnergyChunkSymbol( labelString, energyType, modelViewTransform ) {
-    var labelText = new Text( labelString, {
-      font: LEGEND_ENTRY_FONT,
-      maxWidth: 100
-    } );
-
-    // The EnergyChunks that are created here are not going to be used in the simulation, they are only needed for the
-    // EnergyChunkNodes that are displayed in the legend.
-    var iconNode = new EnergyChunkNode(
-      new EnergyChunk( energyType, Vector2.ZERO, Vector2.ZERO, new Property( true ) ),
-      modelViewTransform
-    );
-
-    return new HBox( {
-      children: [ iconNode, labelText ],
-      spacing: 10
-    } );
-  }
-
-  energyFormsAndChanges.register( 'EnergyChunkLegend', EnergyChunkLegend );
-
-  return inherit( Panel, EnergyChunkLegend );
+  return energyFormsAndChanges.register( 'EnergyChunkLegend', EnergyChunkLegend );
 } );
 
