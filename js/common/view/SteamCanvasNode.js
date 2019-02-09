9952e039bc229e77e000896e7ddedc6d9a6d12cf
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-08 17:47:01 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/common/view/SteamCanvasNode.js
+++ js/common/view/SteamCanvasNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * A node for the column of steam that appears when the temperature is high enough.
@@ -6,24 +6,25 @@
  * @author Chris Klusendorf (PhET Interactive Simulations)
  * @author John Blanco (PhET Interactive Simulations)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Range = require( 'DOT/Range' );
-  var Util = require( 'DOT/Util' );
+  const CanvasNode = require( 'SCENERY/nodes/CanvasNode' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Range = require( 'DOT/Range' );
+  const Util = require( 'DOT/Util' );
 
   // constants
-  var STEAMING_RANGE = 10; // number of degrees Kelvin over which steam is visible
-  var STEAM_BUBBLE_SPEED_RANGE = new Range( 100, 125 ); // in screen coords (~ pixels) / second
-  var STEAM_BUBBLE_DIAMETER_RANGE = new Range( 20, 50 ); // in screen coords (~ pixels)
-  var MAX_STEAM_BUBBLE_HEIGHT = 300;
-  var STEAM_BUBBLE_RATE_RANGE = new Range( 20, 40 ); // bubbles per second
-  var STEAM_BUBBLE_GROWTH_RATE = 0.2; // proportion per second
-  var MAX_STEAM_BUBBLE_OPACITY = 0.7; // proportion, 0 to 1
+  const STEAMING_RANGE = 10; // number of degrees Kelvin over which steam is visible
+  const STEAM_BUBBLE_SPEED_RANGE = new Range( 100, 125 ); // in screen coords (~ pixels) / second
+  const STEAM_BUBBLE_DIAMETER_RANGE = new Range( 20, 50 ); // in screen coords (~ pixels)
+  const MAX_STEAM_BUBBLE_HEIGHT = 300;
+  const STEAM_BUBBLE_RATE_RANGE = new Range( 20, 40 ); // bubbles per second
+  const STEAM_BUBBLE_GROWTH_RATE = 0.2; // proportion per second
+  const MAX_STEAM_BUBBLE_OPACITY = 0.7; // proportion, 0 to 1
+
+  class SteamCanvasNode extends CanvasNode {
 
   /**
    * @param {Rectangle} containerOutlineRect - the outline of the container
@@ -32,12 +33,9 @@ define( function( require ) {
    * @param {number} fluidBoilingPoint
    * @param {Color} steamColor
    * @param {Object} [options]
-   * @constructor
    */
-  function SteamCanvasNode( containerOutlineRect, fluidLevelProperty, temperatureProperty, fluidBoilingPoint, steamColor, options ) {
-
-    var self = this;
-    CanvasNode.call( this, options );
+  constructor( containerOutlineRect, fluidLevelProperty, temperatureProperty, fluidBoilingPoint, steamColor, options ) {
+    super( options );
 
     // @private
     this.containerOutlineRect = containerOutlineRect;
@@ -57,7 +55,7 @@ define( function( require ) {
     this.steamBubbleImageCanvas = document.createElement( 'canvas' );
     this.steamBubbleImageCanvas.width = STEAM_BUBBLE_DIAMETER_RANGE.max;
     this.steamBubbleImageCanvas.height = STEAM_BUBBLE_DIAMETER_RANGE.max;
-    var context = this.steamBubbleImageCanvas.getContext( '2d' );
+    const context = this.steamBubbleImageCanvas.getContext( '2d' );
 
     // draw a steam bubble centered in the steam bubble image canvas
     context.fillStyle = this.steamColor.toCSS();
@@ -73,25 +71,20 @@ define( function( require ) {
     context.fill();
 
     // update the appearance of the water as the level changes
-    this.fluidLevelProperty.link( function( fluidLevel ) {
-      self.steamOrigin = self.containerOutlineRect.minY * fluidLevel;
+    this.fluidLevelProperty.link( fluidLevel => {
+      this.steamOrigin = this.containerOutlineRect.minY * fluidLevel;
     } );
 
     this.mutate( options );
   }
-
-  energyFormsAndChanges.register( 'SteamCanvasNode', SteamCanvasNode );
-
-  return inherit( CanvasNode, SteamCanvasNode, {
 
     /**
      * Renders the steam bubbles on the canvas node.
      * @param {CanvasRenderingContext2D} context
      * @private
      */
-    renderSteam: function( context ) {
-      var i = 0;
-      var steamingProportion = 0;
+    renderSteam( context ) {
+      let steamingProportion = 0;
       if ( this.fluidBoilingPoint - this.temperatureProperty.value < STEAMING_RANGE ) {
 
         // the water is emitting some amount of steam - set the proportionate amount
@@ -101,9 +94,9 @@ define( function( require ) {
 
       // add any new steam bubbles
       if ( steamingProportion > 0 ) {
-        var bubblesToProduceCalc =
+        const bubblesToProduceCalc =
           ( STEAM_BUBBLE_RATE_RANGE.min + STEAM_BUBBLE_RATE_RANGE.getLength() * steamingProportion ) * this.dt;
-        var bubblesToProduce = Math.floor( bubblesToProduceCalc );
+        let bubblesToProduce = Math.floor( bubblesToProduceCalc );
 
         this.bubbleProductionRemainder += bubblesToProduceCalc - bubblesToProduce;
 
@@ -112,15 +105,15 @@ define( function( require ) {
           this.bubbleProductionRemainder -= Math.floor( this.bubbleProductionRemainder );
         }
 
-        for ( i = 0; i < bubblesToProduce; i++ ) {
-          var steamBubbleDiameter = STEAM_BUBBLE_DIAMETER_RANGE.min +
+        for ( let i = 0; i < bubblesToProduce; i++ ) {
+          const steamBubbleDiameter = STEAM_BUBBLE_DIAMETER_RANGE.min +
                                     phet.joist.random.nextDouble() * STEAM_BUBBLE_DIAMETER_RANGE.getLength();
-          var steamBubbleCenterXPos = this.containerOutlineRect.centerX +
-                                      ( phet.joist.random.nextDouble() - 0.5 ) *
-                                      ( this.containerOutlineRect.width - steamBubbleDiameter );
+          const steamBubbleCenterXPos = this.containerOutlineRect.centerX +
+                                        ( phet.joist.random.nextDouble() - 0.5 ) *
+                                        ( this.containerOutlineRect.width - steamBubbleDiameter );
 
           // bubbles are invisible to start; they will fade in
-          var steamBubble = {
+          const steamBubble = {
             x: steamBubbleCenterXPos,
             y: this.steamOrigin,
             radius: steamBubbleDiameter / 2,
@@ -131,10 +124,10 @@ define( function( require ) {
       }
 
       // update the position and appearance of the existing steam bubbles
-      var steamBubbleSpeed = STEAM_BUBBLE_SPEED_RANGE.min + steamingProportion * STEAM_BUBBLE_SPEED_RANGE.getLength();
-      var unfilledBeakerHeight = this.containerOutlineRect.height + this.steamOrigin;
+      const steamBubbleSpeed = STEAM_BUBBLE_SPEED_RANGE.min + steamingProportion * STEAM_BUBBLE_SPEED_RANGE.getLength();
+      const unfilledBeakerHeight = this.containerOutlineRect.height + this.steamOrigin;
 
-      for ( i = 0; i < this.steamBubbles.length; i++ ) {
+      for ( let i = 0; i < this.steamBubbles.length; i++ ) {
 
         // float the bubbles upward from the beaker
         this.steamBubbles[ i ].y += -this.dt * steamBubbleSpeed;
@@ -152,30 +145,28 @@ define( function( require ) {
         // update position of floating bubbles
         else if ( this.steamBubbles[ i ].y < this.containerOutlineRect.minY ) {
           this.steamBubbles[ i ].radius = this.steamBubbles[ i ].radius * 2 * ( 1 + ( STEAM_BUBBLE_GROWTH_RATE * this.dt ) ) / 2;
-          var distanceFromCenterX = this.steamBubbles[ i ].x;
+          const distanceFromCenterX = this.steamBubbles[ i ].x;
 
           // give bubbles some lateral drift motion
           this.steamBubbles[ i ].x += distanceFromCenterX * 0.2 * this.dt;
 
           // fade the bubble as it reaches the end of its range
-          var heightFraction = ( this.containerOutlineRect.minY - this.steamBubbles[ i ].y ) / MAX_STEAM_BUBBLE_HEIGHT;
+          const heightFraction = ( this.containerOutlineRect.minY - this.steamBubbles[ i ].y ) / MAX_STEAM_BUBBLE_HEIGHT;
           this.steamBubbles[ i ].opacity = ( 1 - heightFraction ) * MAX_STEAM_BUBBLE_OPACITY;
         }
 
         // fade new bubbles in
         else {
-          var distanceFromWater = this.steamOrigin - this.steamBubbles[ i ].y;
-          var opacityFraction = Util.clamp( distanceFromWater / ( unfilledBeakerHeight / 4 ), 0, 1 );
+          const distanceFromWater = this.steamOrigin - this.steamBubbles[ i ].y;
+          const opacityFraction = Util.clamp( distanceFromWater / ( unfilledBeakerHeight / 4 ), 0, 1 );
           this.steamBubbles[ i ].opacity = opacityFraction * MAX_STEAM_BUBBLE_OPACITY;
         }
         this.drawSteamBubble( context, this.steamBubbles[ i ] );
       }
 
-      //TODO: why is paintCanvas being called when the sim is paused and the beakers are being dragged?
-      // see https://github.com/phetsims/energy-forms-and-changes/issues/190#issuecomment-457755051
-      // clear the most recent dt value after it is used
+      // clear the most recent dt value after it is used, see https://github.com/phetsims/energy-forms-and-changes/issues/190
       this.dt = 0;
-    },
+    }
 
     /*
      * Draws a steam bubble.
@@ -183,7 +174,7 @@ define( function( require ) {
      * @param {Object} steamBubble
      * @private
      */
-    drawSteamBubble: function( context, steamBubble ) {
+    drawSteamBubble( context, steamBubble ) {
       context.globalAlpha = steamBubble.opacity;
       context.drawImage(
         this.steamBubbleImageCanvas,
@@ -192,31 +183,33 @@ define( function( require ) {
         steamBubble.radius * 2,
         steamBubble.radius * 2
       );
-    },
+    }
 
     /**
      * Paints the steam on the canvas node.
      * @param {CanvasRenderingContext2D} context
      * @public
      */
-    paintCanvas: function( context ) {
+    paintCanvas( context ) {
       this.renderSteam( context );
-    },
+    }
 
     /**
      * @public
      */
-    reset: function() {
+    reset() {
       this.steamBubbles.length = 0;
-    },
+    }
 
     /**
      * @public
      * @param {number} dt - the change in time
      */
-    step: function( dt ) {
+    step( dt ) {
       this.dt = dt;
       this.invalidatePaint();
     }
-  } );
+  }
+
+  return energyFormsAndChanges.register( 'SteamCanvasNode', SteamCanvasNode );
 } );
