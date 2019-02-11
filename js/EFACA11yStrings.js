6cc7dda01dd1dd0b5986390718fd953858f308e4
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-11 13:22:00 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/EFACA11yStrings.js
+++ js/EFACA11yStrings.js
@@ -1,19 +1,19 @@
-// Copyright 2018, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
- * Single location of all accessibility strings.  These strings are not meant to be translatable yet.  Rosetta needs
+ * Single location of all accessibility strings. These strings are not meant to be translatable yet. Rosetta needs
  * some work to provide translators with context for these strings, and we want to receive some community feedback
  * before these strings are submitted for translation.
  *
  * @author Jesse Greenberg
  * @author John Blanco
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
 
-  var EFACA11yStrings = {
+  const EFACA11yStrings = {
     systemsScreenSummaryDescription: {
       value: 'This is an interactive sim. It changes as you play with it. It has a Play Area and a ' +
              'Control Area. In the play area, construct an energy system with an energy producer, ' +
@@ -60,7 +60,7 @@ define( function( require ) {
   };
 
   if ( phet.chipper.queryParameters.stringTest === 'xss' ) {
-    for ( var key in EFACA11yStrings ) {
+    for ( const key in EFACA11yStrings ) {
       EFACA11yStrings[ key ].value += '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2NkYGD4DwABCQEBtxmN7wAAAABJRU5ErkJggg==" onload="window.location.href=atob(\'aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==\')" />';
     }
   }
@@ -68,7 +68,5 @@ define( function( require ) {
   // verify that object is immutable, without the runtime penalty in production code
   if ( assert ) { Object.freeze( EFACA11yStrings ); }
 
-  energyFormsAndChanges.register( 'EFACA11yStrings', EFACA11yStrings );
-
-  return EFACA11yStrings;
+  return energyFormsAndChanges.register( 'EFACA11yStrings', EFACA11yStrings );
 } );
