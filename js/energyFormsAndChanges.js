78adda721ec975ebc3cf58a532168cc91e1174a7
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-11 13:24:14 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/energyFormsAndChanges.js
+++ js/energyFormsAndChanges.js
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, University of Colorado Boulder
+// Copyright 2016-2019, University of Colorado Boulder
 
 /**
  * Create energyFormsAndChanges namespace
  *
- * @author  Jesse Greenberg
- * @author  Andrew Adare
+ * @author Jesse Greenberg
+ * @author Andrew Adare
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var Namespace = require( 'PHET_CORE/Namespace' );
+  const Namespace = require( 'PHET_CORE/Namespace' );
 
   return new Namespace( 'energyFormsAndChanges' );
 } );
