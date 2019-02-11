a91018f8b220f82a8ca36ec584e45733e7f4cbfa
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-11 13:11:38 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/common/EFACConstants.js
+++ js/common/EFACConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * shared constants for the Energy Forms and Changes simulation
@@ -6,54 +6,54 @@
  * @author John Blanco
  */
 
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
 
   // modules
-  var Color = require( 'SCENERY/util/Color' );
-  var LinearFunction = require( 'DOT/LinearFunction' );
-  var ScreenView = require( 'JOIST/ScreenView' );
-  var Util = require( 'DOT/Util' );
-  var Vector2 = require( 'DOT/Vector2' );
+  const Color = require( 'SCENERY/util/Color' );
+  const LinearFunction = require( 'DOT/LinearFunction' );
+  const ScreenView = require( 'JOIST/ScreenView' );
+  const Util = require( 'DOT/Util' );
+  const Vector2 = require( 'DOT/Vector2' );
 
   // constants used for creating projections that have a 3D-ish look.
-  var Z_TO_X_OFFSET_MULTIPLIER = -0.25;
-  var Z_TO_Y_OFFSET_MULTIPLIER = -0.25;
+  const Z_TO_X_OFFSET_MULTIPLIER = -0.25;
+  const Z_TO_Y_OFFSET_MULTIPLIER = -0.25;
 
   // physical temperature constants
-  var ROOM_TEMPERATURE = 296; // in degrees Kelvin
-  var WATER_FREEZING_POINT_TEMPERATURE = 273.15; // in degrees Kelvin
+  const ROOM_TEMPERATURE = 296; // in degrees Kelvin
+  const WATER_FREEZING_POINT_TEMPERATURE = 273.15; // in degrees Kelvin
 
   // constants that define physical parameters of various rectangular objects.
-  var BRICK_DENSITY = 3300; // in kg/m^3, source = design document plus some tweaking to keep chunk numbers reasonable
-  var BRICK_SPECIFIC_HEAT = 840; // in J/kg-K, source = design document
-  var BLOCK_SURFACE_WIDTH = 0.045;
+  const BRICK_DENSITY = 3300; // in kg/m^3, source = design document plus some tweaking to keep chunk numbers reasonable
+  const BRICK_SPECIFIC_HEAT = 840; // in J/kg-K, source = design document
+  const BLOCK_SURFACE_WIDTH = 0.045;
 
   // brick constants needed for energy chunk mapping
-  var BRICK_ENERGY_AT_ROOM_TEMPERATURE = Math.pow( BLOCK_SURFACE_WIDTH, 3 ) * BRICK_DENSITY * BRICK_SPECIFIC_HEAT * ROOM_TEMPERATURE; // In joules.
-  var BRICK_ENERGY_AT_FREEZING_TEMPERATURE = Math.pow( BLOCK_SURFACE_WIDTH, 3 ) * BRICK_DENSITY * BRICK_SPECIFIC_HEAT * WATER_FREEZING_POINT_TEMPERATURE; // In joules.
+  const BRICK_ENERGY_AT_ROOM_TEMPERATURE = Math.pow( BLOCK_SURFACE_WIDTH, 3 ) * BRICK_DENSITY * BRICK_SPECIFIC_HEAT * ROOM_TEMPERATURE; // In joules.
+  const BRICK_ENERGY_AT_FREEZING_TEMPERATURE = Math.pow( BLOCK_SURFACE_WIDTH, 3 ) * BRICK_DENSITY * BRICK_SPECIFIC_HEAT * WATER_FREEZING_POINT_TEMPERATURE; // In joules.
 
   // constants for temperature-energy mapping functions
-  var LOW_ENERGY_FOR_MAP_FUNCTION = BRICK_ENERGY_AT_FREEZING_TEMPERATURE;
-  var HIGH_ENERGY_FOR_MAP_FUNCTION = BRICK_ENERGY_AT_ROOM_TEMPERATURE;
+  const LOW_ENERGY_FOR_MAP_FUNCTION = BRICK_ENERGY_AT_FREEZING_TEMPERATURE;
+  const HIGH_ENERGY_FOR_MAP_FUNCTION = BRICK_ENERGY_AT_ROOM_TEMPERATURE;
 
   // TODO: Why are these fixed numbers?  Seems like they should be calculated.  jbphet 1/18/2019
-  var NUM_ENERGY_CHUNKS_IN_BRICK_AT_FREEZING = 1.25;
-  var NUM_ENERGY_CHUNKS_IN_BRICK_AT_ROOM_TEMP = 2.4; // close to rounding to 3 so that little energy needed to transfer first chunk
+  const NUM_ENERGY_CHUNKS_IN_BRICK_AT_FREEZING = 1.25;
+  const NUM_ENERGY_CHUNKS_IN_BRICK_AT_ROOM_TEMP = 2.4; // close to rounding to 3 so that little energy needed to transfer first chunk
 
   // time values for normal and fast-forward motion
-  var FRAMES_PER_SECOND = 60.0;
-  var SIM_TIME_PER_TICK_NORMAL = 1 / FRAMES_PER_SECOND;
+  const FRAMES_PER_SECOND = 60.0;
+  const SIM_TIME_PER_TICK_NORMAL = 1 / FRAMES_PER_SECOND;
 
   // colors
-  var NOMINAL_WATER_OPACITY = 0.75;
-  var FIRST_SCREEN_BACKGROUND_COLOR = new Color( 249, 244, 205 );
-  var SECOND_SCREEN_BACKGROUND_COLOR = new Color( 249, 244, 205 );
+  const NOMINAL_WATER_OPACITY = 0.75;
+  const FIRST_SCREEN_BACKGROUND_COLOR = new Color( 249, 244, 205 );
+  const SECOND_SCREEN_BACKGROUND_COLOR = new Color( 249, 244, 205 );
 
   // mapping function that maps the energy to the number of energy chunks
-  var MAP_ENERGY_TO_NUM_CHUNKS = new LinearFunction(
+  const MAP_ENERGY_TO_NUM_CHUNKS = new LinearFunction(
     LOW_ENERGY_FOR_MAP_FUNCTION,
     HIGH_ENERGY_FOR_MAP_FUNCTION,
     NUM_ENERGY_CHUNKS_IN_BRICK_AT_FREEZING,
@@ -61,14 +61,14 @@ define( function( require ) {
   );
 
   // mapping function that maps the number of chunks of energy to the energy value
-  var MAP_NUM_CHUNKS_TO_ENERGY = new LinearFunction(
+  const MAP_NUM_CHUNKS_TO_ENERGY = new LinearFunction(
     NUM_ENERGY_CHUNKS_IN_BRICK_AT_FREEZING,
     NUM_ENERGY_CHUNKS_IN_BRICK_AT_ROOM_TEMP,
     LOW_ENERGY_FOR_MAP_FUNCTION,
     HIGH_ENERGY_FOR_MAP_FUNCTION
   );
 
-  var EFACConstants = {
+  const EFACConstants = {
 
     // physical temperature constants
     ROOM_TEMPERATURE: 296, // in degrees Kelvin
@@ -87,14 +87,14 @@ define( function( require ) {
     SIM_TIME_PER_TICK_NORMAL: 1 / FRAMES_PER_SECOND,
     MAX_HEAT_EXCHANGE_TIME_STEP: SIM_TIME_PER_TICK_NORMAL,
 
-    MAP_Z_TO_XY_OFFSET: function( zValue ) {
+    MAP_Z_TO_XY_OFFSET: zValue => {
       return new Vector2( zValue * Z_TO_X_OFFSET_MULTIPLIER, zValue * Z_TO_Y_OFFSET_MULTIPLIER );
     },
 
     // for comparing temperatures
     SIGNIFICANT_TEMPERATURE_DIFFERENCE: 1E-3, // in degrees Kelvin
 
-    ENERGY_TO_NUM_CHUNKS_MAPPER: function( energy ) {
+    ENERGY_TO_NUM_CHUNKS_MAPPER: energy => {
       return Math.max( Util.roundSymmetric( MAP_ENERGY_TO_NUM_CHUNKS( energy ) ), 0 );
     },
 
@@ -170,7 +170,5 @@ define( function( require ) {
     SCREEN_LAYOUT_BOUNDS: ScreenView.DEFAULT_LAYOUT_BOUNDS
   };
 
-  energyFormsAndChanges.register( 'EFACConstants', EFACConstants );
-
-  return EFACConstants;
+  return energyFormsAndChanges.register( 'EFACConstants', EFACConstants );
 } );
