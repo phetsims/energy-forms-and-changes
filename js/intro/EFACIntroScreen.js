c8fce0d62e5c7254d0f9f276ead62baa03c5ff10
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-11 13:04:21 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/intro/EFACIntroScreen.js
+++ js/intro/EFACIntroScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * the 'Intro' screen in the Energy Forms and Changes simulation
@@ -6,48 +6,41 @@
  * @author John Blanco
  * @author Martin Veillette (Berea College)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
-  var EFACIntroModel = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/EFACIntroModel' );
-  var EFACIntroScreenView = require( 'ENERGY_FORMS_AND_CHANGES/intro/view/EFACIntroScreenView' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var Image = require( 'SCENERY/nodes/Image' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Property = require( 'AXON/Property' );
-  var Screen = require( 'JOIST/Screen' );
+  const EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
+  const EFACIntroModel = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/EFACIntroModel' );
+  const EFACIntroScreenView = require( 'ENERGY_FORMS_AND_CHANGES/intro/view/EFACIntroScreenView' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Image = require( 'SCENERY/nodes/Image' );
+  const Property = require( 'AXON/Property' );
+  const Screen = require( 'JOIST/Screen' );
 
   // strings
-  var introString = require( 'string!ENERGY_FORMS_AND_CHANGES/intro' );
+  const introString = require( 'string!ENERGY_FORMS_AND_CHANGES/intro' );
 
   // images
-  var introScreenIcon = require( 'image!ENERGY_FORMS_AND_CHANGES/intro_screen_icon.png' );
+  const introScreenIcon = require( 'image!ENERGY_FORMS_AND_CHANGES/intro_screen_icon.png' );
 
-  /**
-   * @param {Tandem} tandem
-   * @constructor
-   */
-  function EFACIntroScreen( tandem ) {
+  class EFACIntroScreen extends Screen {
 
-    var options = {
-      name: introString,
-      backgroundColorProperty: new Property( EFACConstants.FIRST_SCREEN_BACKGROUND_COLOR ),
-      homeScreenIcon: new Image( introScreenIcon ),
-      tandem: tandem
-    };
+    /**
+     * @param {Tandem} tandem
+     */
+    constructor( tandem ) {
 
-    Screen.call( this,
-      function() {
-        return new EFACIntroModel();
-      },
-      function( model ) {
-        return new EFACIntroScreenView( model );
-      },
-      options );
+      const options = {
+        name: introString,
+        backgroundColorProperty: new Property( EFACConstants.FIRST_SCREEN_BACKGROUND_COLOR ),
+        homeScreenIcon: new Image( introScreenIcon ),
+        tandem: tandem
+      };
+
+      super( () => new EFACIntroModel(), model => new EFACIntroScreenView( model ), options );
+    }
   }
 
-  energyFormsAndChanges.register( 'EFACIntroScreen', EFACIntroScreen );
-  return inherit( Screen, EFACIntroScreen );
+  return energyFormsAndChanges.register( 'EFACIntroScreen', EFACIntroScreen );
 } );
