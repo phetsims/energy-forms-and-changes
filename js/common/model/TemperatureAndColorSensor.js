19bd257115ff122da8bd3f1cae2df9ace294ed45
chrisklus <christopher.klusendorf@colorado.edu>
2019-02-11 12:42:22 -0700

    Convert to ES6, see https://github.com/phetsims/energy-forms-and-changes/issues/211

--- js/common/model/TemperatureAndColorSensor.js
+++ js/common/model/TemperatureAndColorSensor.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2018, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * a model element senses the temperature and color of the model at its current position, and can be moved around
@@ -6,48 +6,44 @@
  * @author John Blanco
  */
 
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
-  var energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Property = require( 'AXON/Property' );
-  var UserMovableModelElement = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/UserMovableModelElement' );
+  const EFACConstants = require( 'ENERGY_FORMS_AND_CHANGES/common/EFACConstants' );
+  const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
+  const Property = require( 'AXON/Property' );
+  const UserMovableModelElement = require( 'ENERGY_FORMS_AND_CHANGES/intro/model/UserMovableModelElement' );
 
-  /**
-   * @param {EFACIntroModel} model
-   * @param {Vector2} initialPosition
-   * @param {boolean} initiallyActive
-   * @constructor
-   */
-  function TemperatureAndColorSensor( model, initialPosition, initiallyActive ) {
-    UserMovableModelElement.call( this, initialPosition );
+  class TemperatureAndColorSensor extends UserMovableModelElement {
 
-    // @private
-    this.model = model;
+    /**
+     * @param {EFACIntroModel} model
+     * @param {Vector2} initialPosition
+     * @param {boolean} initiallyActive
+     */
+    constructor( model, initialPosition, initiallyActive ) {
+      super( initialPosition );
 
-    // @public (read-only) {Property.<number>}
-    this.sensedTemperatureProperty = new Property( EFACConstants.ROOM_TEMPERATURE );
+      // @private
+      this.model = model;
 
-    // @public (read-only) {Property.<Color>}
-    this.sensedElementColorProperty = new Property( EFACConstants.TEMPERATURE_SENSOR_INACTIVE_COLOR );
+      // @public (read-only) {Property.<number>}
+      this.sensedTemperatureProperty = new Property( EFACConstants.ROOM_TEMPERATURE );
 
-    // @public (read-only) {Property.<boolean>} - used to control visibility in the view
-    this.activeProperty = new Property( initiallyActive );
-  }
+      // @public (read-only) {Property.<Color>}
+      this.sensedElementColorProperty = new Property( EFACConstants.TEMPERATURE_SENSOR_INACTIVE_COLOR );
 
-  energyFormsAndChanges.register( 'TemperatureAndColorSensor', TemperatureAndColorSensor );
-
-  return inherit( UserMovableModelElement, TemperatureAndColorSensor, {
+      // @public (read-only) {Property.<boolean>} - used to control visibility in the view
+      this.activeProperty = new Property( initiallyActive );
+    }
 
     /**
      * @public
      */
-    step: function() {
+    step() {
       if ( this.activeProperty.value ) {
-        var temperatureAndColor = this.model.getTemperatureAndColorAtLocation( this.positionProperty.value );
+        const temperatureAndColor = this.model.getTemperatureAndColorAtLocation( this.positionProperty.value );
         this.sensedTemperatureProperty.set( temperatureAndColor.temperature );
         this.sensedElementColorProperty.set( temperatureAndColor.color );
       }
@@ -55,17 +51,18 @@ define( function( require ) {
         this.sensedTemperatureProperty.set( EFACConstants.ROOM_TEMPERATURE );
         this.sensedElementColorProperty.set( EFACConstants.TEMPERATURE_SENSOR_INACTIVE_COLOR );
       }
-    },
+    }
 
     /**
      * @public
      */
-    reset: function() {
+    reset() {
       this.sensedTemperatureProperty.reset();
       this.sensedElementColorProperty.reset();
       this.activeProperty.reset();
     }
-  } );
+  }
 
+  return energyFormsAndChanges.register( 'TemperatureAndColorSensor', TemperatureAndColorSensor );
 } );
 
