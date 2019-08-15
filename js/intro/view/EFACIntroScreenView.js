8501929b1b4161525053cea310fe36f847056a73
chrisklus <christopher.klusendorf@colorado.edu>
2019-08-15 10:35:04 -0400

    Convert to BooleanProperty, see https://github.com/phetsims/energy-forms-and-changes/issues/258

--- js/intro/view/EFACIntroScreenView.js
+++ js/intro/view/EFACIntroScreenView.js
@@ -41,7 +41,6 @@ define( require => {
   const Panel = require( 'SUN/Panel' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const PlayPauseStepButtonGroup = require( 'ENERGY_FORMS_AND_CHANGES/common/view/PlayPauseStepButtonGroup' );
-  const Property = require( 'AXON/Property' );
   const Rectangle = require( 'SCENERY/nodes/Rectangle' );
   const ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   const ScreenView = require( 'JOIST/ScreenView' );
@@ -569,7 +568,7 @@ define( require => {
       // allow each to be independently scaled. The EnergyChunk that is created here is not going to be used in the
       // simulation, it is only needed for the EnergyChunkNode that is displayed in the show/hide energy chunks toggle.
       const energyChunkNode = new EnergyChunkNode(
-        new EnergyChunk( EnergyType.THERMAL, Vector2.ZERO, Vector2.ZERO, new Property( true ) ),
+        new EnergyChunk( EnergyType.THERMAL, Vector2.ZERO, Vector2.ZERO, new BooleanProperty( true ) ),
         modelViewTransform
       );
       energyChunkNode.pickable = false;
