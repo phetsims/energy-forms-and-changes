a091d0b8036ffe48ec41084b3759aa7cd88fb55b
chrisklus <christopher.klusendorf@colorado.edu>
2019-10-01 21:22:30 -0400

    Fix formatting

--- js/intro/model/BlockType.js
+++ js/intro/model/BlockType.js
@@ -12,14 +12,14 @@ define( require => {
   const energyFormsAndChanges = require( 'ENERGY_FORMS_AND_CHANGES/energyFormsAndChanges' );
   const Enumeration = require( 'PHET_CORE/Enumeration' );
 
-// @public
-const BlockType = new Enumeration( [ 'IRON', 'BRICK' ], {
-  beforeFreeze: BlockType => {
-    BlockType.getTandemName = blockType => {
-      return blockType.toString().toLowerCase() + 'Block';
-    };
-  }
-} );
+  // @public
+  const BlockType = new Enumeration( [ 'IRON', 'BRICK' ], {
+    beforeFreeze: BlockType => {
+      BlockType.getTandemName = blockType => {
+        return blockType.toString().toLowerCase() + 'Block';
+      };
+    }
+  } );
 
   return energyFormsAndChanges.register( 'BlockType', BlockType );
 } );
