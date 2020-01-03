b2115d5c500d8f1cd2d7d9415b29ca68af01bcff
phet-dev <phetadmin@gmail.com>
2020-01-03 03:10:43 -0700

    update copyright dates from daily grunt work

--- js/systems/model/SystemsModel.js
+++ js/systems/model/SystemsModel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * model for the 'Systems' screen of the Energy Forms And Changes simulation
