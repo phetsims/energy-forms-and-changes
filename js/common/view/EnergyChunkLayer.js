6550db3353170091258c82467ac016669b83c462
phet-dev <phetadmin@gmail.com>
2020-02-06 03:14:30 -0700

    update copyright dates from daily grunt work

--- js/common/view/EnergyChunkLayer.js
+++ js/common/view/EnergyChunkLayer.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * This node monitors the comings and goings of energy chunks on a observable list and adds/removes nodes that
