6ef4cb0c690bf0b0b9f97e898075479aa783cb5d
phet-dev <phetadmin@gmail.com>
2020-02-12 03:14:37 -0700

    update copyright dates from daily grunt work

--- js/systems/view/GeneratorNode.js
+++ js/systems/view/GeneratorNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * a Scenery Node that represents and electrical generator in the view
