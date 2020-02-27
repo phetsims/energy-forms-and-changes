7fead587650efd31965c1fdcea068b19e83cd727
Amy Rouinfar <amy.rouinfar@colorado.edu>
2020-02-27 15:09:44 -0700

    added overrides for LabelNode https://github.com/phetsims/energy-forms-and-changes/issues/314

--- js/phet-io/energy-forms-and-changes-phet-io-elements-overrides.js
+++ js/phet-io/energy-forms-and-changes-phet-io-elements-overrides.js
@@ -1,194 +1,217 @@
-
 /* eslint-disable */
 window.phet.phetio.phetioElementsOverrides =
-  {
-    "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.energyProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.fluidProportionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.positionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.temperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.blockGroup.archetype.energyProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.blockGroup.archetype.positionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.blockGroup.archetype.temperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.energyChunksVisibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.isPlayingProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.leftBurner.heatCoolLevelProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.leftBurner.positionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.linkedHeatersProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.rightBurner.heatCoolLevelProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.rightBurner.positionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer1.sensedElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer1.sensedTemperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer2.sensedElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer2.sensedTemperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer3.sensedElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer3.sensedTemperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer4.sensedElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.model.thermometer4.sensedTemperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.beakerProxyNodeGroup.archetype.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.blockNodeGroup.archetype.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.controlPanel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer1Node.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer1Node.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer2Node.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer2Node.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer3Node.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer3Node.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer4Node.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometer4Node.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.introScreen.view.thermometerStorageAreaNode.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyChunksVisibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyConverters.generator.wheelRotationalVelocityProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyConverters.solarPanel.energyOutputRateProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyConvertersCarousel.targetElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.biker.crankAngularVelocityProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.biker.energyChunksRemainingProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.biker.targetCrankAngularVelocityProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.faucetAndWater.flowProportionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.sun.cloudinessProportionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.sun.sunProportionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySources.teaKettle.energyProductionRateProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energySourcesCarousel.targetElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyUsers.beakerHeater.waterBeaker.temperatureProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyUsers.fan.angularVelocityProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyUsers.fluorescentBulb.litProportionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyUsers.incandescentBulb.litProportionProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.energyUsersCarousel.targetElementNameProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.model.isPlayingProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.controlPanel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energyConverterSelectorPanel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySourceSelectorPanel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.bikerNode.controlPanel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.bikerNode.feedMeButton.enabledProperty": {
-      "phetioFeatured": false
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.bikerNode.feedMeButton.visibleProperty": {
-      "phetioFeatured": false
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.faucetAndWaterNode.faucetSettingProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.faucetAndWaterNode.pickableProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.sunNode.cloudsPanel.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energySources.teaKettleNode.heaterSettingProperty": {
-      "phetioFeatured": true
-    },
-    "energyFormsAndChanges.systemsScreen.view.energyUserSelectorPanel.visibleProperty": {
-      "phetioFeatured": true
-    }
-  };
+{
+  "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.energyProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.fluidProportionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.positionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.temperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.blockGroup.archetype.energyProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.blockGroup.archetype.positionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.blockGroup.archetype.temperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.energyChunksVisibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.isPlayingProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.leftBurner.heatCoolLevelProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.leftBurner.positionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.linkedHeatersProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.rightBurner.heatCoolLevelProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.rightBurner.positionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer1.sensedElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer1.sensedTemperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer2.sensedElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer2.sensedTemperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer3.sensedElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer3.sensedTemperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer4.sensedElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.model.thermometer4.sensedTemperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.beakerProxyNodeGroup.archetype.labelNode.textProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.beakerProxyNodeGroup.archetype.labelNode.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.beakerProxyNodeGroup.archetype.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.blockNodeGroup.archetype.labelNode.textProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.blockNodeGroup.archetype.labelNode.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.blockNodeGroup.archetype.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.controlPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer1Node.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer1Node.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer2Node.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer2Node.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer3Node.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer3Node.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer4Node.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometer4Node.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.introScreen.view.thermometerStorageAreaNode.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyChunksVisibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyConverters.generator.wheelRotationalVelocityProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyConverters.solarPanel.energyOutputRateProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyConvertersCarousel.targetElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.biker.crankAngularVelocityProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.biker.energyChunksRemainingProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.biker.targetCrankAngularVelocityProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.faucetAndWater.flowProportionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.sun.cloudinessProportionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.sun.sunProportionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySources.teaKettle.energyProductionRateProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energySourcesCarousel.targetElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyUsers.beakerHeater.waterBeaker.temperatureProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyUsers.fan.angularVelocityProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyUsers.fluorescentBulb.litProportionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyUsers.incandescentBulb.litProportionProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.energyUsersCarousel.targetElementNameProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.model.isPlayingProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.controlPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energyConverterSelectorPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energyConverters.generatorNode.labelNode.textProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energyConverters.generatorNode.labelNode.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySourceSelectorPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.bikerNode.controlPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.bikerNode.feedMeButton.enabledProperty": {
+    "phetioFeatured": false
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.bikerNode.feedMeButton.visibleProperty": {
+    "phetioFeatured": false
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.faucetAndWaterNode.faucetSettingProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.faucetAndWaterNode.pickableProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.sunNode.cloudsPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energySources.teaKettleNode.heaterSettingProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energyUserSelectorPanel.visibleProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energyUsers.beakerHeaterNode.beakerProxyNode.labelNode.textProperty": {
+    "phetioFeatured": true
+  },
+  "energyFormsAndChanges.systemsScreen.view.energyUsers.beakerHeaterNode.beakerProxyNode.labelNode.visibleProperty": {
+    "phetioFeatured": true
+  }
+};
