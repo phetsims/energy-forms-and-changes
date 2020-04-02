947b850c74dacb4290366ef6721b8a513cd67c5b
zepumph <michael.kauzmann@colorado.edu>
2020-04-01 16:12:06 -0800

    move values from phet.phetio to phet.phetio.preload, https://github.com/phetsims/phet-io/issues/1636

--- js/phet-io/energy-forms-and-changes-phet-io-elements-overrides.js
+++ js/phet-io/energy-forms-and-changes-phet-io-elements-overrides.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-window.phet.phetio.phetioElementsOverrides =
+window.phet.phetio.preload.phetioElementsOverrides =
   {
     "energyFormsAndChanges.introScreen.model.beakerGroup.archetype.energyProperty": {
       "phetioFeatured": true
