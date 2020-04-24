d0cf51095dbd4fe1eb3227aaffeb5df0a267590a
samreid <reids@colorado.edu>
2020-04-24 10:41:50 -0600

    grunt modulify to use SimLauncher locks, see https://github.com/phetsims/joist/issues/621

--- images/solar_panel_post_png.js
+++ images/solar_panel_post_png.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
-const img = new Image();
-window.phetImages.push( img );
-img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAABGCAYAAACUsCfoAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAMJJREFUeNrs3M0JhDAQBtDEH7QcS9jS7GBbsxdPIuqqJ1kIyJLLyvtgmCGBwCP3iX3fv4ui6MqyDEftc7gzp7Jt21nXOcdZ5jeHKsbY1XX9ats23K2maZLwdV3Dsixnv87fPefdL2+lv+7hAQcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcH/8dUx6KIeZ7P3QnTNIVxHB+/HGPP8BFgANTMW0xuY9ZuAAAAAElFTkSuQmCC';
-export default img;
+import SimLauncher from '../../joist/js/SimLauncher.js';
+const image = new Image();
+const unlock = SimLauncher.createLock( image );
+image.onload = unlock;
+image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAABGCAYAAACUsCfoAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAMJJREFUeNrs3M0JhDAQBtDEH7QcS9jS7GBbsxdPIuqqJ1kIyJLLyvtgmCGBwCP3iX3fv4ui6MqyDEftc7gzp7Jt21nXOcdZ5jeHKsbY1XX9ats23K2maZLwdV3Dsixnv87fPefdL2+lv+7hAQcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcH/8dUx6KIeZ7P3QnTNIVxHB+/HGPP8BFgANTMW0xuY9ZuAAAAAElFTkSuQmCC';
+export default image;
