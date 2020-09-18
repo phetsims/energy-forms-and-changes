900c6d418680fac2dc115e0fc7f4f5044a06d2ff
jbphet <john.blanco@colorado.edu>
2020-09-18 13:10:22 -0600

    docs

--- js/common/view/BeakerSteamCanvasNode.js
+++ js/common/view/BeakerSteamCanvasNode.js
@@ -1,7 +1,7 @@
 // Copyright 2018-2020, University of Colorado Boulder
 
 /**
- * A node for the column of steam that appears when the temperature is high enough.
+ * A node for the column of steam that rises out of a beaker when the temperature of the contained liquid is high enough.
  *
  * @author Chris Klusendorf (PhET Interactive Simulations)
  * @author John Blanco (PhET Interactive Simulations)
