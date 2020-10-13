fbd153eea9d493e14f77f07d6ce82be6e7ba3152
jbphet <john.blanco@colorado.edu>
2020-10-13 12:56:38 -0600

    updated QA credits, see https://github.com/phetsims/energy-forms-and-changes/issues/216

--- js/energy-forms-and-changes-main.js
+++ js/energy-forms-and-changes-main.js
@@ -25,7 +25,8 @@ const simOptions = {
     leadDesign: 'Noah Podolefsky, Amy Rouinfar',
     softwareDevelopment: 'Andrew Adare, John Blanco, Chris Klusendorf',
     team: 'Trish Loeblein, Emily B. Moore, Ariel Paul, Kathy Perkins, and in cooperation with the Next-Lab project',
-    qualityAssurance: 'Steele Dalton, Megan Lai, Liam Mulhall, Laura Rea, Jacob Romero, Katie Woessner, Kelly Wurtz, Bryan Yoelin',
+    qualityAssurance: 'Logan Bray, Steele Dalton, Megan Lai, Brooklyn Lash, Liam Mulhall, Devon Quispe,<br>' +
+                      'Laura Rea, Jacob Romero, Katie Woessner, Kelly Wurtz, Bryan Yoelin',
     graphicArts: 'Cheryl McCutchan, Mariah Hermsmeyer, Megan Lai',
     thanks: ''
   }
