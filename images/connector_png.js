459cd3c7680689617af22fd839e9dfa4ed76c165
zepumph <michael.kauzmann@colorado.edu>
2021-01-22 12:51:55 -0900

    use asyncLoader instead of simLauncher, plus grunt update on accident, https://github.com/phetsims/joist/issues/687

--- images/connector_png.js
+++ images/connector_png.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 
 const image = new Image();
-const unlock = simLauncher.createLock( image );
+const unlock = asyncLoader.createLock( image );
 image.onload = unlock;
 image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAABICAYAAACJFMdwAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAV1JREFUeNrsm8FthTAMhh0SQEJIMMp7IzABHaGdBJikXYEJGKGMAhcuBKiNSvWqXqsqrn5LURJOH9ZHCCg29BBt2z5zVxtjSgoojuOYuOuZ7+26Zj6BS4YdnHM37inEYHjy3o/cV8w7ObkYRdGQZVmw0A/wt2VZBh7ejehRFMVrHMekIdZ1pXmeXxwD13mek5ZI05Q467VLkqSUiaYQZmetJW3gwuz4wZQ7UAUuzCe4tozrBlftOFSBKv9dFY3reERKA+AABzjAfzfc9QWtDlygt21TBS3MJ/i+7wD/M3AZaAP/chyqQBWoAlWgim5wlXsVOA7HoQpUgSpQBarg9wTA4TiWQ6gCVaAKVIEq2GRhkwXHsRxClRBUYehJG7gwC3jvvX+SA1pKoKX1Z21B13Xv1loNpQbyshybprlfxz4qzvrAWQ+6uIMzPfKwkvk3yquchlsZGPePcpoPAQYAstydVJJ0xdwAAAAASUVORK5CYII=';
 export default image;
--- images/solar_panel_post_png.js
+++ images/solar_panel_post_png.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 
 const image = new Image();
-const unlock = simLauncher.createLock( image );
+const unlock = asyncLoader.createLock( image );
 image.onload = unlock;
 image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAD4AAABGCAYAAACUsCfoAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAMJJREFUeNrs3M0JhDAQBtDEH7QcS9jS7GBbsxdPIuqqJ1kIyJLLyvtgmCGBwCP3iX3fv4ui6MqyDEftc7gzp7Jt21nXOcdZ5jeHKsbY1XX9ats23K2maZLwdV3Dsixnv87fPefdL2+lv+7hAQcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcH/8dUx6KIeZ7P3QnTNIVxHB+/HGPP8BFgANTMW0xuY9ZuAAAAAElFTkSuQmCC';
 export default image;
