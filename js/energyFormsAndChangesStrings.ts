af1c10bf6b3608f429b0697f99374782bece0c46
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:28 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/energyFormsAndChangesStrings.ts
@@ -0,0 +1,57 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import energyFormsAndChanges from './energyFormsAndChanges.js';
+
+type StringsType = {
+  'energy-forms-and-changes': {
+    'title': string
+  },
+  'intro': string,
+  'systems': string,
+  'brick': string,
+  'iron': string,
+  'energySymbols': string,
+  'water': string,
+  'oliveOil': string,
+  'generator': string,
+  'clouds': string,
+  'lots': string,
+  'none': string,
+  'feedMe': string,
+  'normal': string,
+  'fastForward': string,
+  'formsOfEnergy': string,
+  'mechanical': string,
+  'electrical': string,
+  'thermal': string,
+  'light': string,
+  'linkHeaters': string,
+  'chemical': string,
+  'energyChunkLabel': string,
+  'a11y': {
+    'systemsScreenSummaryDescription': string,
+    'systemsScreenInteractionHint': string,
+    'energySystem': string,
+    'energySystemHelpText': string,
+    'waterFaucet': string,
+    'sun': string,
+    'teaKettle': string,
+    'cyclist': string,
+    'electricalGenerator': string,
+    'solarPanel': string,
+    'beakerOfWater': string,
+    'incandescentLightBulb': string,
+    'fluorescentLightBulb': string
+  }
+};
+
+const energyFormsAndChangesStrings = getStringModule( 'ENERGY_FORMS_AND_CHANGES' ) as StringsType;
+
+energyFormsAndChanges.register( 'energyFormsAndChangesStrings', energyFormsAndChangesStrings );
+
+export default energyFormsAndChangesStrings;
