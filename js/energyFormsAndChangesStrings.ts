97d15c78a02eb0e520313cbf75bc52f3860da938
Sam Reid <reids@colorado.edu>
2022-05-07 09:33:36 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/energyFormsAndChangesStrings.ts
+++ js/energyFormsAndChangesStrings.ts
@@ -9,44 +9,44 @@ import energyFormsAndChanges from './energyFormsAndChanges.js';
 
 type StringsType = {
   'energy-forms-and-changes': {
-    'title': string
-  },
-  'intro': string,
-  'systems': string,
-  'brick': string,
-  'iron': string,
-  'energySymbols': string,
-  'water': string,
-  'oliveOil': string,
-  'generator': string,
-  'clouds': string,
-  'lots': string,
-  'none': string,
-  'feedMe': string,
-  'normal': string,
-  'fastForward': string,
-  'formsOfEnergy': string,
-  'mechanical': string,
-  'electrical': string,
-  'thermal': string,
-  'light': string,
-  'linkHeaters': string,
-  'chemical': string,
-  'energyChunkLabel': string,
+    'title': string;
+  };
+  'intro': string;
+  'systems': string;
+  'brick': string;
+  'iron': string;
+  'energySymbols': string;
+  'water': string;
+  'oliveOil': string;
+  'generator': string;
+  'clouds': string;
+  'lots': string;
+  'none': string;
+  'feedMe': string;
+  'normal': string;
+  'fastForward': string;
+  'formsOfEnergy': string;
+  'mechanical': string;
+  'electrical': string;
+  'thermal': string;
+  'light': string;
+  'linkHeaters': string;
+  'chemical': string;
+  'energyChunkLabel': string;
   'a11y': {
-    'systemsScreenSummaryDescription': string,
-    'systemsScreenInteractionHint': string,
-    'energySystem': string,
-    'energySystemHelpText': string,
-    'waterFaucet': string,
-    'sun': string,
-    'teaKettle': string,
-    'cyclist': string,
-    'electricalGenerator': string,
-    'solarPanel': string,
-    'beakerOfWater': string,
-    'incandescentLightBulb': string,
-    'fluorescentLightBulb': string
+    'systemsScreenSummaryDescription': string;
+    'systemsScreenInteractionHint': string;
+    'energySystem': string;
+    'energySystemHelpText': string;
+    'waterFaucet': string;
+    'sun': string;
+    'teaKettle': string;
+    'cyclist': string;
+    'electricalGenerator': string;
+    'solarPanel': string;
+    'beakerOfWater': string;
+    'incandescentLightBulb': string;
+    'fluorescentLightBulb': string;
   }
 };
 
