7852c8da589d7c475b957b2b25df6ab64c90f28e
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/energyFormsAndChangesStrings.ts
+++ js/energyFormsAndChangesStrings.ts
@@ -5,48 +5,85 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import energyFormsAndChanges from './energyFormsAndChanges.js';
 
 type StringsType = {
   'energy-forms-and-changes': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'intro': string;
+  'introProperty': TReadOnlyProperty<string>;
   'systems': string;
+  'systemsProperty': TReadOnlyProperty<string>;
   'brick': string;
+  'brickProperty': TReadOnlyProperty<string>;
   'iron': string;
+  'ironProperty': TReadOnlyProperty<string>;
   'energySymbols': string;
+  'energySymbolsProperty': TReadOnlyProperty<string>;
   'water': string;
+  'waterProperty': TReadOnlyProperty<string>;
   'oliveOil': string;
+  'oliveOilProperty': TReadOnlyProperty<string>;
   'generator': string;
+  'generatorProperty': TReadOnlyProperty<string>;
   'clouds': string;
+  'cloudsProperty': TReadOnlyProperty<string>;
   'lots': string;
+  'lotsProperty': TReadOnlyProperty<string>;
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'feedMe': string;
+  'feedMeProperty': TReadOnlyProperty<string>;
   'normal': string;
+  'normalProperty': TReadOnlyProperty<string>;
   'fastForward': string;
+  'fastForwardProperty': TReadOnlyProperty<string>;
   'formsOfEnergy': string;
+  'formsOfEnergyProperty': TReadOnlyProperty<string>;
   'mechanical': string;
+  'mechanicalProperty': TReadOnlyProperty<string>;
   'electrical': string;
+  'electricalProperty': TReadOnlyProperty<string>;
   'thermal': string;
+  'thermalProperty': TReadOnlyProperty<string>;
   'light': string;
+  'lightProperty': TReadOnlyProperty<string>;
   'linkHeaters': string;
+  'linkHeatersProperty': TReadOnlyProperty<string>;
   'chemical': string;
+  'chemicalProperty': TReadOnlyProperty<string>;
   'energyChunkLabel': string;
+  'energyChunkLabelProperty': TReadOnlyProperty<string>;
   'a11y': {
     'systemsScreenSummaryDescription': string;
+    'systemsScreenSummaryDescriptionProperty': TReadOnlyProperty<string>;
     'systemsScreenInteractionHint': string;
+    'systemsScreenInteractionHintProperty': TReadOnlyProperty<string>;
     'energySystem': string;
+    'energySystemProperty': TReadOnlyProperty<string>;
     'energySystemHelpText': string;
+    'energySystemHelpTextProperty': TReadOnlyProperty<string>;
     'waterFaucet': string;
+    'waterFaucetProperty': TReadOnlyProperty<string>;
     'sun': string;
+    'sunProperty': TReadOnlyProperty<string>;
     'teaKettle': string;
+    'teaKettleProperty': TReadOnlyProperty<string>;
     'cyclist': string;
+    'cyclistProperty': TReadOnlyProperty<string>;
     'electricalGenerator': string;
+    'electricalGeneratorProperty': TReadOnlyProperty<string>;
     'solarPanel': string;
+    'solarPanelProperty': TReadOnlyProperty<string>;
     'beakerOfWater': string;
+    'beakerOfWaterProperty': TReadOnlyProperty<string>;
     'incandescentLightBulb': string;
+    'incandescentLightBulbProperty': TReadOnlyProperty<string>;
     'fluorescentLightBulb': string;
+    'fluorescentLightBulbProperty': TReadOnlyProperty<string>;
   }
 };
 
