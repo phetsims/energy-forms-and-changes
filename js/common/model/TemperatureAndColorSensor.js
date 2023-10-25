95e21a8fda714eded8119d4d6c34a2f80e90f392
phet-dev <phetadmin@gmail.com>
2023-10-25 03:19:25 -0600

    update copyright dates from daily grunt work

--- js/common/model/TemperatureAndColorSensor.js
+++ js/common/model/TemperatureAndColorSensor.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * a model element that senses the temperature and color of the model at its current position, and can be moved around
--- js/common/model/UserMovableModelElement.js
+++ js/common/model/UserMovableModelElement.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * base class for model elements that can be moved around by the user
