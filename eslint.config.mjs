62309588cf79970241c4b052f779237b678b69d9
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:22 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for energy-forms-and-changes.
+ * ESLint configuration for energy-forms-and-changes.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
