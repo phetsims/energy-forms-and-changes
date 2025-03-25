8a319ea032430ff30160dd857630bdc9739ecdf8
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:44:08 -0600

    runnable *QueryParameters.js files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/EFACQueryParameters.js
+++ js/common/EFACQueryParameters.js
@@ -7,6 +7,7 @@
  * @author Chris Klusendorf (PhET Interactive Simulations)
  */
 
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import energyFormsAndChanges from '../energyFormsAndChanges.js';
 import EFACConstants from './EFACConstants.js';
 
